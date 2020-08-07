fe734c0ab804a4258178a90efa39fb625954d7dc
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-07 14:26:00 +0200

    :art: Inherit from ViewSet and not ReadOnlyModelViewSet

--- src/openpersonen/api/views/ingeschrevenpersoon.py
+++ src/openpersonen/api/views/ingeschrevenpersoon.py
@@ -1,15 +1,34 @@
 from rest_framework.status import HTTP_200_OK
 from rest_framework.response import Response
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ViewSet
 
 from openpersonen.api.serializers import IngeschrevenPersoonSerializer
 from openpersonen.api.test_data import test_data
 
 
-class IngeschrevenPersoon(ReadOnlyModelViewSet):
+class IngeschrevenPersoon(ViewSet):
 
     lookup_field = "burgerservicenummer"
     serializer_class = IngeschrevenPersoonSerializer
+
+    def get_serializer_context(self):
+        """
+        Extra context provided to the serializer class.
+        """
+        return {
+            'request': self.request,
+            'format': self.format_kwarg,
+            'view': self
+        }
+
+    def get_serializer(self, *args, **kwargs):
+        """
+        Return the serializer instance that should be used for validating and
+        deserializing input, and for serializing output.
+        """
+        serializer_class = self.serializer_class
+        kwargs['context'] = self.get_serializer_context()
+        return serializer_class(*args, **kwargs)
 
     def list(self, request, *args, **kwargs):
 
