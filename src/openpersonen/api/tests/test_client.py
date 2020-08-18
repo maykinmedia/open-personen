d5d2df602980748136f539b2a82141f5919bf4c8
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-18 17:21:53 +0200

    :construction: Add unit tests asserts

--- src/openpersonen/api/tests/test_client.py
+++ src/openpersonen/api/tests/test_client.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.test import TestCase
 
 from openpersonen.api.client import Client
@@ -14,7 +15,16 @@ class TestClient(TestCase):
         response = self.client.get_gezinssituatie_op_adres_aanvrager(test_bsn)
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn('antwoord', str(response.content))
+        response_content = str(response.content)
+        self.assertIn('antwoord', response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['organisatie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['applicatie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['administratie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['gebruiker'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['organisatie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['applicatie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['administratie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['gebruiker'], response_content)
 
     def test_get_kinderen_van_aanvrager(self):
         test_bsn = 123456789
@@ -22,7 +32,16 @@ class TestClient(TestCase):
         response = self.client.get_kinderen_van_aanvrager(test_bsn)
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn('antwoord', str(response.content))
+        response_content = str(response.content)
+        self.assertIn('antwoord', response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['organisatie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['applicatie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['administratie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['gebruiker'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['organisatie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['applicatie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['administratie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['gebruiker'], response_content)
 
     def test_get_natuurlijk_persoon(self):
         test_bsn = 123456789
@@ -30,7 +49,16 @@ class TestClient(TestCase):
         response = self.client.get_natuurlijk_persoon(test_bsn)
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn('antwoord', str(response.content))
+        response_content = str(response.content)
+        self.assertIn('antwoord', response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['organisatie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['applicatie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['administratie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['gebruiker'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['organisatie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['applicatie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['administratie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['gebruiker'], response_content)
 
     def test_get_vestiging(self):
         test_bsn = 123456789
@@ -38,4 +66,13 @@ class TestClient(TestCase):
         response = self.client.get_vestiging(test_bsn)
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn('antwoord', str(response.content))
+        response_content = str(response.content)
+        self.assertIn('antwoord', response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['organisatie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['applicatie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['administratie'], response_content)
+        self.assertIn(settings.STUF_BG_ONTVANGER['gebruiker'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['organisatie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['applicatie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['administratie'], response_content)
+        self.assertIn(settings.STUF_BG_ZENDER['gebruiker'], response_content)
