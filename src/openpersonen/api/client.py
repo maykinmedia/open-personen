78f9e343ed4a55ce3379b0550b2c5509458ffd2e
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-18 10:44:56 +0200

    :construction: Add request templates for four client calls and update client

--- src/openpersonen/api/client.py
+++ src/openpersonen/api/client.py
@@ -6,15 +6,21 @@ from django.conf import settings
 class Client:
 
     def get_gezinssituatie_op_adres_aanvrager(self, bsn):
-        pass
+        requests.post(settings.STUF_BG_URL,
+                      data=loader.render_to_string('GezinssituatieOpAdresAanvrager.xml', {'bsn': bsn}),
+                      headers=settings.STUF_BG_HEADERS)
 
     def get_kinderen_van_aanvrager(self, bsn):
-        pass
+        requests.post(settings.STUF_BG_URL,
+                      data=loader.render_to_string('KinderenVanAanvrager.xml', {'bsn': bsn}),
+                      headers=settings.STUF_BG_HEADERS)
 
     def get_natuurlijk_persoon(self, bsn):
         requests.post(settings.STUF_BG_URL,
-                      data=loader.render_to_string('get_natuurlijk_persoon.xml', {'bsn': bsn}),
+                      data=loader.render_to_string('NatuurlijkPersoon.xml', {'bsn': bsn}),
                       headers=settings.STUF_BG_HEADERS)
 
     def get_vestiging(self, vestigings_nummer):
-        pass
+        requests.post(settings.STUF_BG_URL,
+                      data=loader.render_to_string('Vestiging.xml', {'vestigingsNummer': vestigings_nummer}),
+                      headers=settings.STUF_BG_HEADERS)
