36b0ccb403426ec70acfa5f848aa321963ae67aa
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-18 16:48:26 +0200

    :construction: Update client and test client

--- src/openpersonen/api/tests/test_client.py
+++ src/openpersonen/api/tests/test_client.py
@@ -14,7 +14,7 @@ class TestClient(TestCase):
         response = self.client.get_gezinssituatie_op_adres_aanvrager(test_bsn)
 
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'antwoord')
+        self.assertIn('antwoord', str(response.content))
 
     def test_get_kinderen_van_aanvrager(self):
         test_bsn = 123456789
@@ -22,7 +22,7 @@ class TestClient(TestCase):
         response = self.client.get_kinderen_van_aanvrager(test_bsn)
 
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'antwoord')
+        self.assertIn('antwoord', str(response.content))
 
     def test_get_natuurlijk_persoon(self):
         test_bsn = 123456789
@@ -30,7 +30,7 @@ class TestClient(TestCase):
         response = self.client.get_natuurlijk_persoon(test_bsn)
 
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'antwoord')
+        self.assertIn('antwoord', str(response.content))
 
     def test_get_vestiging(self):
         test_bsn = 123456789
@@ -38,4 +38,4 @@ class TestClient(TestCase):
         response = self.client.get_vestiging(test_bsn)
 
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'antwoord')
+        self.assertIn('antwoord', str(response.content))
