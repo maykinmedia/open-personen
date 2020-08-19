8fe328a307216eaa65a53c5c5d71a7905f7d47c3
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-19 15:23:30 +0200

    :construction: Fix naming mistakes in serializers

--- src/openpersonen/api/serializers/ingeschreven_personen.py
+++ src/openpersonen/api/serializers/ingeschreven_personen.py
@@ -7,7 +7,7 @@ from .gezags_verhouding import GezagsVerhoudingSerializer
 from .in_onderzoek import IngeschrevenPersoonInOnderzoekSerializer
 from .kiesrecht import KiesrechtSerializer
 from .nationaliteit import NationaliteitSerializer
-from .opschorting_bijhouding import OpschortingBijhouding
+from .opschorting_bijhouding import OpschortingBijhoudingSerializer
 from .overlijden import OverlijdenSerializer
 from .persoon import PersoonSerializer
 from .verblijf_plaats import VerblijfPlaatsSerializer
@@ -22,7 +22,7 @@ class IngeschrevenPersoonSerializer(PersoonSerializer):
     kiesrecht = KiesrechtSerializer(required=False)
     inOnderzoek = IngeschrevenPersoonInOnderzoekSerializer(required=False)
     nationaliteit = NationaliteitSerializer(many=True, required=False)
-    opschortingBijhouding = OpschortingBijhouding(required=False)
+    opschortingBijhouding = OpschortingBijhoudingSerializer(required=False)
     overlijden = OverlijdenSerializer(required=False)
     verblijfplaats = VerblijfPlaatsSerializer(required=False)
     gezagsverhouding = GezagsVerhoudingSerializer(required=False)
--- src/openpersonen/api/serializers/kind.py
+++ src/openpersonen/api/serializers/kind.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-from .in_onderzoek import KinderInOnderzoek
+from .in_onderzoek import KindInOnderzoekSerializer
 from .persoon import PersoonSerializer
 
 
 class KindSerializer(PersoonSerializer):
     leeftijd = serializers.IntegerField(max_value=999, required=False)
-    inOnderzoek = KinderInOnderzoek(required=False)
+    inOnderzoek = KindInOnderzoekSerializer(required=False)
