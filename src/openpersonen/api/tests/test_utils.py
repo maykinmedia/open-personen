0bfb3a93bb5cc8973bea1eda146f9ab74330bb12
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-21 16:50:38 +0200

    Add unit test for utils

---
+++ src/openpersonen/api/tests/test_utils.py
@@ -0,0 +1,26 @@
+from collections import OrderedDict
+
+from django.test import TestCase
+
+from openpersonen.api.utils import convert_empty_instances
+
+
+class TestConvertEmptyInstances(TestCase):
+
+    def test_convert_empty_instances(self):
+        original_dict = {
+            'key': OrderedDict(),
+            'nested_dict': {
+                'nested_key': OrderedDict(),
+            }
+        }
+        expected_dict = {
+            'key': None,
+            'nested_dict': {
+                'nested_key': None,
+            }
+        }
+
+        convert_empty_instances(original_dict)
+
+        self.assertEqual(original_dict, expected_dict)
