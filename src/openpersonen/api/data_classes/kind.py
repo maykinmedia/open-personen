d16151a1f66e314ba99ad70033efe9c93fcd0a24
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-24 11:52:20 +0200

    Add unit test for retrieving a kind

--- src/openpersonen/api/data_classes/kind.py
+++ src/openpersonen/api/data_classes/kind.py
@@ -1,5 +1,11 @@
 from dataclasses import dataclass
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
+import xmltodict
+
+from openpersonen.api.client import client
+from openpersonen.api.utils import convert_empty_instances
 from .in_onderzoek import KindInOnderzoek
 from .persoon import Persoon
 
@@ -8,3 +14,79 @@ from .persoon import Persoon
 class Kind(Persoon):
     leeftijd: int
     inOnderzoek: KindInOnderzoek
+
+    @staticmethod
+    def get_instance_dict(response):
+        dict_object = xmltodict.parse(response.content)
+
+        antwoord_dict_object = dict_object['env:Envelope']['env:Body']['npsLa01']['BG:antwoord']['object']
+
+        kind_dict = {
+            "burgerservicenummer": antwoord_dict_object['BG:inp.bsn'],
+            "geheimhoudingPersoonsgegevens": True,
+            "naam": {
+                "geslachtsnaam": antwoord_dict_object['BG:geslachtsnaam'],
+                "voorletters": antwoord_dict_object['BG:voorletters'],
+                "voornamen": antwoord_dict_object['BG:voornamen'],
+                "voorvoegsel": antwoord_dict_object['BG:voorvoegselGeslachtsnaam'],
+                "inOnderzoek": {
+                    "geslachtsnaam": bool(antwoord_dict_object['BG:geslachtsnaam']),
+                    "voornamen": bool(antwoord_dict_object['BG:voornamen']),
+                    "voorvoegsel": bool(antwoord_dict_object['BG:voorvoegselGeslachtsnaam']),
+                    "datumIngangOnderzoek": {
+                        "dag": 0,
+                        "datum": "string",
+                        "jaar": 0,
+                        "maand": 0
+                    }
+                }
+            },
+            "geboorte": {
+                "datum": {
+                    "dag": antwoord_dict_object['BG:geboortedatum'][6:8],
+                    "datum": antwoord_dict_object['BG:geboortedatum'],
+                    "jaar": antwoord_dict_object['BG:geboortedatum'][0:4],
+                    "maand": antwoord_dict_object['BG:geboortedatum'][4:6]
+                },
+                "land": {
+                    "code": "string",
+                    "omschrijving": antwoord_dict_object['BG:inp.geboorteLand']
+                },
+                "plaats": {
+                    "code": "string",
+                    "omschrijving": antwoord_dict_object['BG:inp.geboorteplaats']
+                },
+                "inOnderzoek": {
+                    "datum": True,
+                    "land": True,
+                    "plaats": True,
+                    "datumIngangOnderzoek": {
+                        "dag": 0,
+                        "datum": "string",
+                        "jaar": 0,
+                        "maand": 0
+                    }
+                }
+            },
+            "leeftijd": relativedelta(datetime.now(),
+                                      datetime.strptime(antwoord_dict_object['BG:geboortedatum'], '%Y%m%d')).years,
+            "inOnderzoek": {
+                "burgerservicenummer": True,
+                "datumIngangOnderzoek": {
+                    "dag": 0,
+                    "datum": "string",
+                    "jaar": 0,
+                    "maand": 0
+                }
+            }
+        }
+
+        convert_empty_instances(kind_dict)
+
+        return kind_dict
+
+    @classmethod
+    def retrieve(cls, bsn):
+        response = client.get_kinderen_van_aanvrager(bsn)
+        instance_dict = cls.get_instance_dict(response)
+        return cls(**instance_dict)
---
+++ src/openpersonen/api/tests/views/test_kind.py
@@ -0,0 +1,49 @@
+import requests_mock
+from django.conf import settings
+from django.template import loader
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+from openpersonen.api.tests.test_data import kind_data
+from openpersonen.accounts.models import User
+
+
+class TestKind(APITestCase):
+
+    def test_kind_without_token(self):
+        response = self.client.get(reverse('kinderen-list',
+                                           kwargs={
+                                               'ingeschrevenpersonen_burgerservicenummer': 000000000
+                                           }))
+        self.assertEqual(response.status_code, 401)
+
+    def test_kind_with_token(self):
+        user = User.objects.create(username='test')
+        token = Token.objects.create(user=user)
+        response = self.client.get(reverse('kinderen-list',
+                                           kwargs={
+                                               'ingeschrevenpersonen_burgerservicenummer': 000000000
+                                           }),
+                                   HTTP_AUTHORIZATION=f'Token {token.key}')
+        self.assertEqual(response.status_code, 200)
+
+    @requests_mock.Mocker()
+    def test_retrieve_kind(self, post_mock):
+        post_mock.post(
+            settings.STUF_BG_URL,
+            content=bytes(loader.render_to_string('ResponseNatuurlijkPersoonSoapUIWithVariables.xml'),
+                          encoding='utf-8')
+        )
+
+        user = User.objects.create(username='test')
+        token = Token.objects.create(user=user)
+        response = self.client.get(reverse('kinderen-list',
+                                           kwargs={
+                                               'ingeschrevenpersonen_burgerservicenummer': 000000000
+                                           }) + '/1',
+                                   HTTP_AUTHORIZATION=f'Token {token.key}')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(post_mock.called)
+        self.assertEqual(response.json(), kind_data)
