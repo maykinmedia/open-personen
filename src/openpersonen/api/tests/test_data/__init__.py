2746ea0edea10d8d270f7f8e05b164b4822ee0c8
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-24 11:52:20 +0200

    Add unit test for retrieving a kind

--- src/openpersonen/api/tests/test_data/__init__.py
+++ src/openpersonen/api/tests/test_data/__init__.py
@@ -1,1 +1,2 @@
 from .ingeschreven_persoon import ingeschreven_persoon_data
+from .kind import kind_data
---
+++ src/openpersonen/api/tests/test_data/kind.py
@@ -0,0 +1,58 @@
+kind_data = {
+    "burgerservicenummer": "123456780",
+    "geheimhoudingPersoonsgegevens": True,
+    "naam": {
+        "geslachtsnaam": "Vliet",
+        "voorletters": "I.",
+        "voornamen": "Isaak",
+        "voorvoegsel": "van",
+        "inOnderzoek": {
+            "geslachtsnaam": True,
+            "voornamen": True,
+            "voorvoegsel": True,
+            "datumIngangOnderzoek": {
+                "dag": 0,
+                "datum": "string",
+                "jaar": 0,
+                "maand": 0
+            }
+        }
+    },
+    "geboorte": {
+        "datum": {
+            "dag": 15,
+            "datum": "19800915",
+            "jaar": 1980,
+            "maand": 9
+        },
+        "land": {
+            "code": "string",
+            "omschrijving": "6030"
+        },
+        "plaats": {
+            "code": "string",
+            "omschrijving": "Amsterdam"
+        },
+        "inOnderzoek": {
+            "datum": True,
+            "land": True,
+            "plaats": True,
+            "datumIngangOnderzoek": {
+                "dag": 0,
+                "datum": "string",
+                "jaar": 0,
+                "maand": 0
+            }
+        }
+    },
+    "leeftijd": 39,
+    "inOnderzoek": {
+        "burgerservicenummer": True,
+        "datumIngangOnderzoek": {
+            "dag": 0,
+            "datum": "string",
+            "jaar": 0,
+            "maand": 0
+        }
+    }
+}
