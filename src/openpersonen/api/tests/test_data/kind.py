0025838ee2948fa9688bfcc565bd405d1fd88898
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-25 12:40:37 +0200

    :white_check_mark: Fix failing unit tests

--- src/openpersonen/api/tests/test_data/kind.py
+++ src/openpersonen/api/tests/test_data/kind.py
@@ -1,58 +1,70 @@
 kind_retrieve_data = {
+    "_embedded": {
+        "naam": {
+            "_embedded": {
+                "inOnderzoek": {
+                    "_embedded": {
+                        "datumIngangOnderzoek": {
+                            "dag": 0,
+                            "datum": "string",
+                            "jaar": 0,
+                            "maand": 0
+                        }
+                    },
+                    "geslachtsnaam": True,
+                    "voornamen": True,
+                    "voorvoegsel": True
+                }
+            },
+            "geslachtsnaam": "Maykin Kind",
+            "voorletters": "K",
+            "voornamen": "Media Kind",
+            "voorvoegsel": "van"
+        },
+        "geboorte": {
+            "_embedded": {
+                "datum": {
+                    "dag": 15,
+                    "datum": "19990615",
+                    "jaar": 1999,
+                    "maand": 6
+                },
+                "land": {
+                    "code": "string",
+                    "omschrijving": "Nederland"
+                },
+                "plaats": {
+                    "code": "string",
+                    "omschrijving": "Amsterdam"
+                },
+                "inOnderzoek": {
+                    "_embedded": {
+                        "datumIngangOnderzoek": {
+                            "dag": 0,
+                            "datum": "string",
+                            "jaar": 0,
+                            "maand": 0
+                        }
+                    },
+                    "datum": True,
+                    "land": True,
+                    "plaats": True
+                }
+            }
+        },
+        "inOnderzoek": {
+            "_embedded": {
+                "datumIngangOnderzoek": {
+                    "dag": 0,
+                    "datum": "string",
+                    "jaar": 0,
+                    "maand": 0
+                }
+            },
+            "burgerservicenummer": True
+        }
+    },
     "burgerservicenummer": "456789123",
     "geheimhoudingPersoonsgegevens": True,
-    "naam": {
-        "geslachtsnaam": "Maykin Kind",
-        "voorletters": "K",
-        "voornamen": "Media Kind",
-        "voorvoegsel": "van",
-        "inOnderzoek": {
-            "geslachtsnaam": True,
-            "voornamen": True,
-            "voorvoegsel": True,
-            "datumIngangOnderzoek": {
-                "dag": 0,
-                "datum": "string",
-                "jaar": 0,
-                "maand": 0
-            }
-        }
-    },
-    "geboorte": {
-        "datum": {
-            "dag": 15,
-            "datum": "19990615",
-            "jaar": 1999,
-            "maand": 6
-        },
-        "land": {
-            "code": "string",
-            "omschrijving": "Nederland"
-        },
-        "plaats": {
-            "code": "string",
-            "omschrijving": "Amsterdam"
-        },
-        "inOnderzoek": {
-            "datum": True,
-            "land": True,
-            "plaats": True,
-            "datumIngangOnderzoek": {
-                "dag": 0,
-                "datum": "string",
-                "jaar": 0,
-                "maand": 0
-            }
-        }
-    },
-    "leeftijd": 21,
-    "inOnderzoek": {
-        "burgerservicenummer": True,
-        "datumIngangOnderzoek": {
-            "dag": 0,
-            "datum": "string",
-            "jaar": 0,
-            "maand": 0
-        }
-    }
+    "leeftijd": 21
 }
