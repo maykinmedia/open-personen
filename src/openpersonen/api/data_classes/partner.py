c4aa54f1772f2de56425d2366b13c983626c483e
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-25 15:01:43 +0200

    :white_check_mark: Add client function and XML for partner

--- src/openpersonen/api/data_classes/partner.py
+++ src/openpersonen/api/data_classes/partner.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 
+import xmltodict
+
+from openpersonen.api.client import client
 from openpersonen.api.enum import GeslachtsaanduidingChoices, SoortVerbintenis
+from openpersonen.api.utils import convert_empty_instances
 from .aangaan_huwelijk_partnerschap import AangaanHuwelijkPartnerschap
 from .in_onderzoek import PartnerInOnderzoek
 from .persoon import Persoon
@@ -18,3 +22,108 @@ class Partner(Persoon):
 
     def get_soortVerbintenis_display(self):
         return SoortVerbintenis.values[self.soortVerbintenis]
+
+    @staticmethod
+    def get_instance_dict(response):
+        dict_object = xmltodict.parse(response.content)
+
+        antwoord_dict_object = \
+        dict_object['soapenv:Envelope']['soapenv:Body']['ns:npsLa01']['ns:antwoord']['ns:object']['ns:inp.heeftAlsEchtgenootPartner']['ns:gerelateerde']
+
+        partner_dict = {
+            "burgerservicenummer": antwoord_dict_object['ns:inp.bsn'],
+            "geslachtsaanduiding": antwoord_dict_object['ns:geslachtsaanduiding'],
+            "soortVerbintenis": antwoord_dict_object['ns:soortVerbintenis'],
+            "naam": {
+                "geslachtsnaam": antwoord_dict_object['ns:geslachtsnaam'],
+                "voorletters": antwoord_dict_object['ns:voorletters'],
+                "voornamen": antwoord_dict_object['ns:voornamen'],
+                "voorvoegsel": antwoord_dict_object['ns:voorvoegselGeslachtsnaam'],
+                "inOnderzoek": {
+                    "geslachtsnaam": bool(antwoord_dict_object['ns:geslachtsnaam']),
+                    "voornamen": bool(antwoord_dict_object['ns:voornamen']),
+                    "voorvoegsel": bool(antwoord_dict_object['ns:voorvoegselGeslachtsnaam']),
+                    "datumIngangOnderzoek": {
+                        "dag": 0,
+                        "datum": "string",
+                        "jaar": 0,
+                        "maand": 0
+                    }
+                }
+            },
+            "geboorte": {
+                "datum": {
+                    "dag": antwoord_dict_object['ns:geboortedatum'][6:8],
+                    "datum": antwoord_dict_object['ns:geboortedatum'],
+                    "jaar": antwoord_dict_object['ns:geboortedatum'][0:4],
+                    "maand": antwoord_dict_object['ns:geboortedatum'][4:6]
+                },
+                "land": {
+                    "code": "0000",
+                    "omschrijving": antwoord_dict_object['ns:inp.geboorteLand']
+                },
+                "plaats": {
+                    "code": "0000",
+                    "omschrijving": antwoord_dict_object['ns:inp.geboorteplaats']
+                },
+                "inOnderzoek": {
+                    "datum": bool(antwoord_dict_object['ns:geboortedatum']),
+                    "land": bool(antwoord_dict_object['ns:inp.geboorteLand']),
+                    "plaats": bool(antwoord_dict_object['ns:inp.geboorteplaats']),
+                    "datumIngangOnderzoek": {
+                        "dag": 0,
+                        "datum": "string",
+                        "jaar": 0,
+                        "maand": 0
+                    }
+                }
+            },
+            "inOnderzoek": {
+                "burgerservicenummer": bool(antwoord_dict_object['ns:inp.bsn']),
+                "geslachtsaanduiding": bool(antwoord_dict_object['ns:geslachtsaanduiding']),
+                "datumIngangOnderzoek": {
+                    "dag": 0,
+                    "datum": "string",
+                    "jaar": 0,
+                    "maand": 0
+                }
+            },
+            "aangaanHuwelijkPartnerschap": {
+                "datum": {
+                    "dag": 0,
+                    "datum": "string",
+                    "jaar": 0,
+                    "maand": 0
+                },
+                "land": {
+                    "code": "0000",
+                    "omschrijving": "string"
+                },
+                "plaats": {
+                    "code": "0000",
+                    "omschrijving": "string"
+                },
+                "inOnderzoek": {
+                    "datum": True,
+                    "land": True,
+                    "plaats": True,
+                    "datumIngangOnderzoek": {
+                        "dag": 0,
+                        "datum": "string",
+                        "jaar": 0,
+                        "maand": 0
+                    }
+                }
+            },
+            "geheimhoudingPersoonsgegevens": True,
+        }
+
+        convert_empty_instances(partner_dict)
+
+        return partner_dict
+
+    @classmethod
+    def retrieve(cls, bsn):
+        response = client.get_partner(bsn)
+        instance_dict = cls.get_instance_dict(response)
+        return cls(**instance_dict)
