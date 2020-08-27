0ba1c3a6044953451697c309919b1b4bf9b89d32
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-27 12:00:00 +0200

    :construction: Update import statements

--- src/openpersonen/api/tests/views/test_partner.py
+++ src/openpersonen/api/tests/views/test_partner.py
@@ -6,7 +6,7 @@ from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 
 from openpersonen.accounts.models import User
-from openpersonen.api.tests.test_data.partner import partner_retrieve_data
+from openpersonen.api.tests.test_data import partner_retrieve_data
 
 
 class TestPartner(APITestCase):
