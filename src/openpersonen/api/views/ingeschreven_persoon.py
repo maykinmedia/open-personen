eb75ea6b55b2796e9a14b9d1ec37dfaeaf6c2c09
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-28 10:07:39 +0200

    Check passed in query params

--- src/openpersonen/api/views/ingeschreven_persoon.py
+++ src/openpersonen/api/views/ingeschreven_persoon.py
@@ -1,6 +1,6 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import ViewSet
 from vng_api_common.filters import Backend
 
@@ -34,6 +34,53 @@ class IngeschrevenPersoonViewSet(ViewSet):
         kwargs["context"] = self.get_serializer_context()
         return serializer_class(*args, **kwargs)
 
+    def rule_1(self, filters_values_dict):
+        if filters_values_dict.get('geboorte__datum') and filters_values_dict.get('naam__geslachtsnaam'):
+            return True
+
+        filters_values_dict.pop('geboorte__datum', 0)
+        filters_values_dict.pop('naam__geslachtsnaam', 0)
+
+        return False
+
+    def rule_2(self, filters_values_dict):
+        if filters_values_dict.get('verblijfplaats__gemeentevaninschrijving') and \
+            filters_values_dict.get('naam__geslachtsnaam'):
+            return True
+
+        filters_values_dict.pop('verblijfplaats__gemeentevaninschrijving', 0)
+        filters_values_dict.pop('naam__geslachtsnaam', 0)
+
+        return False
+
+    def rule_3(self, filters_values_dict):
+        return bool(filters_values_dict.get('burgerservicenummer'))
+
+    def rule_4(self, filters_values_dict):
+        if filters_values_dict.get('verblijfplaats__postcode') and \
+            filters_values_dict.get('verblijfplaats__huisnummer'):
+            return True
+
+        filters_values_dict.pop('verblijfplaats__postcode', 0)
+        filters_values_dict.pop('verblijfplaats__huisnummer', 0)
+
+        return False
+
+    def rule_5(self, filters_values_dict):
+        if filters_values_dict.get('verblijfplaats__naamopenbareruimte') and \
+            filters_values_dict.get('verblijfplaats__gemeentevaninschrijving') and \
+            filters_values_dict.get('verblijfplaats__huisnummer'):
+            return True
+
+        filters_values_dict.pop('verblijfplaats__naamopenbareruimte', 0)
+        filters_values_dict.pop('verblijfplaats__gemeentevaninschrijving', 0)
+        filters_values_dict.pop('verblijfplaats__huisnummer', 0)
+
+        return False
+
+    def rule_6(self, filters_values_dict):
+        return bool(filters_values_dict.get('verblijfplaats__identificatiecodenummeraanduiding'))
+
     def get_filters_with_values(self):
         filters_with_values = dict()
         filter_keys = self.filter_class.get_filters().keys()
@@ -41,11 +88,21 @@ class IngeschrevenPersoonViewSet(ViewSet):
             if self.request.GET.get(key):
                 filters_with_values[key] = self.request.GET[key]
 
+        if not any([self.rule_1(filters_with_values), self.rule_2(filters_with_values),
+                    self.rule_3(filters_with_values), self.rule_4(filters_with_values),
+                    self.rule_5(filters_with_values), self.rule_6(filters_with_values)]):
+            raise ValueError('Incorrect combination of filters')
+
         return filters_with_values
 
     def list(self, request, *args, **kwargs):
 
-        instances = IngeschrevenPersoon.retrieve(filters=self.get_filters_with_values())
+        try:
+            filters = self.get_filters_with_values()
+        except ValueError as e:
+            return Response(data={'detail': str(e)}, status=HTTP_400_BAD_REQUEST)
+
+        instances = IngeschrevenPersoon.retrieve(filters=filters)
 
         serializer = self.serializer_class(instances, many=True)
 
