d34b87051e59d403c110efdb0d7efe60c724c021
Joeri Bekker <joeri@maykinmedia.nl>
2020-08-31 13:49:41 +0200

    :bug: Fixed dockerhub repo.

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -15,7 +15,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 # Echo script commands
 set -x
 
-REPO=maykinmedia/openpersonen
+REPO=maykinmedia/open-personen
 TAG=${1:-latest}
 
 # Build the image
