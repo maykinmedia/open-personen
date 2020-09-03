7e739a2504f4d38d7c0b7ca2c6d1f45c59897b7a
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-09-03 15:10:08 +0200

    :art: Reformat code with black

--- src/openpersonen/api/migrations/0001_initial.py
+++ src/openpersonen/api/migrations/0001_initial.py
@@ -7,28 +7,35 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='StufBGClient',
+            name="StufBGClient",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('ontvanger_organisatie', models.CharField(max_length=200)),
-                ('ontvanger_administratie', models.CharField(max_length=200)),
-                ('ontvanger_applicatie', models.CharField(max_length=200)),
-                ('ontvanger_gebruiker', models.CharField(max_length=200)),
-                ('zender_organisatie', models.CharField(max_length=200)),
-                ('zender_administratie', models.CharField(max_length=200)),
-                ('zender_applicatie', models.CharField(max_length=200)),
-                ('zender_gebruiker', models.CharField(max_length=200)),
-                ('url', models.URLField()),
-                ('user', models.CharField(max_length=200)),
-                ('password', models.CharField(max_length=200)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("ontvanger_organisatie", models.CharField(max_length=200)),
+                ("ontvanger_administratie", models.CharField(max_length=200)),
+                ("ontvanger_applicatie", models.CharField(max_length=200)),
+                ("ontvanger_gebruiker", models.CharField(max_length=200)),
+                ("zender_organisatie", models.CharField(max_length=200)),
+                ("zender_administratie", models.CharField(max_length=200)),
+                ("zender_applicatie", models.CharField(max_length=200)),
+                ("zender_gebruiker", models.CharField(max_length=200)),
+                ("url", models.URLField()),
+                ("user", models.CharField(max_length=200)),
+                ("password", models.CharField(max_length=200)),
             ],
             options={
-                'verbose_name': 'Stuf BG Client',
+                "verbose_name": "Stuf BG Client",
             },
         ),
     ]
--- src/openpersonen/api/tests/__init__.py
+++ src/openpersonen/api/tests/__init__.py
@@ -2,5 +2,5 @@ from openpersonen.api.models import StufBGClient
 
 # Set up fake url that fits proper url format
 client = StufBGClient.get_solo()
-client.url = 'http://fake.test.url'
+client.url = "http://fake.test.url"
 client.save()
