5c299d78899ef008787b3384d9ebc3cc9de318fc
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-09-03 16:24:14 +0200

    :construction: Add field labels and update admin interface

--- src/openpersonen/api/admin.py
+++ src/openpersonen/api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from solo.admin import SingletonModelAdmin
 
@@ -7,4 +8,38 @@ from .models import StufBGClient
 
 @admin.register(StufBGClient)
 class StufBGClientAdmin(SingletonModelAdmin):
-    pass
+    # Details
+    fieldsets = (
+        (
+            _("Ontvanger"),
+            {
+                "fields": (
+                    "ontvanger_organisatie",
+                    "ontvanger_administratie",
+                    "ontvanger_applicatie",
+                    "ontvanger_gebruiker",
+                )
+            },
+        ),
+        (
+            _("Zender"),
+            {
+                "fields": (
+                    "zender_organisatie",
+                    "zender_administratie",
+                    "zender_applicatie",
+                    "zender_gebruiker",
+                )
+            },
+        ),
+        (
+            _("Service"),
+            {
+                "fields": (
+                    "url",
+                    "user",
+                    "password",
+                )
+            },
+        ),
+    )
