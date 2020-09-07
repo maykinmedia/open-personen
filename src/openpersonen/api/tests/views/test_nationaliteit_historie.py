f8291e88eb97307e25c700af596bf87cf18ca68c
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-09-07 12:49:22 +0200

    :art: Update files with isort and black

--- src/openpersonen/api/tests/views/test_nationaliteit_historie.py
+++ src/openpersonen/api/tests/views/test_nationaliteit_historie.py
@@ -6,11 +6,11 @@ from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
 from openpersonen.accounts.models import User
-from openpersonen.api.tests.test_data import NATIONALITEIT_HISTORIE_DATA
 from openpersonen.api.models import StufBGClient
+from openpersonen.api.tests.test_data import NATIONALITEIT_HISTORIE_DATA
+
 
 class TestNationaliteitHistorie(APITestCase):
-
     def setUp(self):
         self.url = StufBGClient.get_solo().url
 
--- src/openpersonen/api/tests/views/test_partner_historie.py
+++ src/openpersonen/api/tests/views/test_partner_historie.py
@@ -6,11 +6,11 @@ from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
 from openpersonen.accounts.models import User
-from openpersonen.api.tests.test_data import PARTNER_HISTORIE_DATA
 from openpersonen.api.models import StufBGClient
+from openpersonen.api.tests.test_data import PARTNER_HISTORIE_DATA
+
 
 class TestPartnerHistorie(APITestCase):
-
     def setUp(self):
         self.url = StufBGClient.get_solo().url
 
