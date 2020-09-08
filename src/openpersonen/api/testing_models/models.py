3122111812b48b90de481f7f49c3d0a1610f2490
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-09-08 11:11:52 +0200

    :construction: Finish importing other models

--- src/openpersonen/api/testing_models/models.py
+++ src/openpersonen/api/testing_models/models.py
@@ -3,79 +3,79 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Persoon(models.Model):
-    a_nummer_persoon = models.CharField(_("A-nummer persoon"), max_length=10, blank=True)
-    burgerservicenummer_persoon = models.CharField(_("Burgerservicenummer persoon"), max_length=9, blank=True)
+    a_nummer_persoon = models.CharField(_("A-nummer persoon"), max_length=200, blank=True)
+    burgerservicenummer_persoon = models.CharField(_("Burgerservicenummer persoon"), max_length=200, blank=True)
     voornamen_persoon = models.CharField(_("Voornamen persoon"), max_length=200, blank=True)
     adellijke_titel_predikaat_persoon = models.CharField(_("Adellijke titel/predikaat persoon"), max_length=200,
                                                            blank=True)
     voorvoegsel_geslachtsnaam_persoon = models.CharField(_("Voorvoegsel geslachtsnaam persoon"), max_length=200,
                                                          blank=True)
     geslachtsnaam_persoon = models.CharField(_("Geslachtsnaam persoon"), max_length=200, blank=True)
-    geboortedatum_persoon = models.CharField(_("Geboortedatum persoon"), max_length=8, blank=True,
+    geboortedatum_persoon = models.CharField(_("Geboortedatum persoon"), max_length=200, blank=True,
                                                 help_text='Format YYYYMMDD')
     geboorteplaats_persoon = models.CharField(_("Geboorteplaats persoon"), max_length=200, blank=True)
-    geboorteland_persoon = models.CharField(_("Geboorteland persoon"), max_length=4, blank=True,
+    geboorteland_persoon = models.CharField(_("Geboorteland persoon"), max_length=200, blank=True,
                                                help_text='Four digit code')
     geslachtsaanduiding = models.CharField(_("Geslachtsaanduiding"), max_length=1, blank=True,
                                            help_text='M for mannen and V for vrouwen')
-    vorig_a_nummer = models.CharField(_("Vorig A-nummer"), max_length=10, blank=True)
-    volgend_a_nummer = models.CharField(_("Volgend A-nummer"), max_length=10, blank=True)
+    vorig_a_nummer = models.CharField(_("Vorig A-nummer"), max_length=200, blank=True)
+    volgend_a_nummer = models.CharField(_("Volgend A-nummer"), max_length=200, blank=True)
     aanduiding_naamgebruik = models.CharField(_("Aanduiding naamgebruik"), max_length=1, blank=True)
     registergemeente_akte_waaraan_gegevens = models.CharField(
-        _("Registergemeente akte waaraan gegevens over persoon ontleend zijn"), max_length=4, blank=True,
+        _("Registergemeente akte waaraan gegevens over persoon ontleend zijn"), max_length=200, blank=True,
         help_text='Four digit code')
     aktenummer_van_de_akte = models.CharField(
         _("Aktenummer van de akte waaraan gegevens over persoon ontleend zijn"), max_length=7, blank=True)
     gemeente_waar_de_gegevens_over_persoon = models.CharField(
-        _("Gemeente waar de gegevens over persoon aan het document ontleend zijn"), max_length=4, blank=True,
+        _("Gemeente waar de gegevens over persoon aan het document ontleend zijn"), max_length=200, blank=True,
         help_text='Four digit code')
     datum_van_de_ontlening_van_de_gegevens_over_persoon = models.CharField(
-        _("Datum van de ontlening van de gegevens over persoon"), max_length=8, blank=True, help_text='Format YYYYMMDD')
+        _("Datum van de ontlening van de gegevens over persoon"), max_length=200, blank=True, help_text='Format YYYYMMDD')
     beschrijving_van_het_document = models.CharField(
         _("Beschrijving van het document waaraan de gegevens over persoon ontleend zijn"), max_length=200, blank=True)
     aanduiding_gegevens_in_onderzoek = models.CharField(_("Aanduiding gegevens in onderzoek"), max_length=6,
                                                         blank=True, help_text='Six digit code')
-    datum_ingang_onderzoek = models.CharField(_("Datum ingang onderzoek"), max_length=8, blank=True,
+    datum_ingang_onderzoek = models.CharField(_("Datum ingang onderzoek"), max_length=200, blank=True,
                                                  help_text='Format YYYYMMDD')
-    datum_einde_onderzoek = models.CharField(_("Datum einde onderzoek"), max_length=8, blank=True,
+    datum_einde_onderzoek = models.CharField(_("Datum einde onderzoek"), max_length=200, blank=True,
                                                 help_text='Format YYYYMMDD')
     indicatie_onjuist_dan_wel_strijdigheid_met_de_openbare_orde = models.CharField(
         _("Indicatie onjuist dan wel strijdigheid met de openbare orde"), max_length=200, blank=True)
     ingangsdatum_geldigheid_met_betrekking = models.CharField(
-        _("Ingangsdatum geldigheid met betrekking tot de elementen van de categorie Persoon"), max_length=8,
+        _("Ingangsdatum geldigheid met betrekking tot de elementen van de categorie Persoon"), max_length=200,
         blank=True, help_text='Format YYYYMMDD')
     datum_van_opneming_met_betrekking = models.CharField(
-        _("Datum van opneming met betrekking tot de elementen van de categorie Persoon"), max_length=8, blank=True,
+        _("Datum van opneming met betrekking tot de elementen van de categorie Persoon"), max_length=200, blank=True,
         help_text='Format YYYYMMDD')
     rni_deelnemer = models.CharField(_("RNI-deelnemer"), max_length=200, blank=True)
 
 
 class Ouder(models.Model):
-    omschrijving_verdrag = models.CharField(_("Omschrijving verdrag"), max_length=200, blank=True)
-    a_nummer_ouder = models.CharField(_("A-nummer ouder"), max_length=10, blank=True)
-    burgerservicenummer_ouder = models.CharField(_("Burgerservicenummer ouder"), max_length=9, blank=True)
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
+    a_nummer_ouder = models.CharField(_("A-nummer ouder"), max_length=200, blank=True)
+    burgerservicenummer_ouder = models.CharField(_("Burgerservicenummer ouder"), max_length=200, blank=True)
     voornamen_ouder = models.CharField(_("Voornamen ouder"), max_length=200, blank=True)
     adellijke_titel_predikaat_ouder = models.CharField(_("Adellijke titel/predikaat ouder"), max_length=200,
                                                           blank=True)
     voorvoegsel_geslachtsnaam_ouder = models.CharField(_("Voorvoegsel geslachtsnaam ouder"), max_length=200,
                                                         blank=True)
     geslachtsnaam_ouder = models.CharField(_("Geslachtsnaam ouder"), max_length=200, blank=True)
-    geboortedatum_ouder = models.CharField(_("Geboortedatum ouder"), max_length=8, blank=True,
+    geboortedatum_ouder = models.CharField(_("Geboortedatum ouder"), max_length=200, blank=True,
                                               help_text='Format YYYYMMDD')
     geboorteplaats_ouder = models.CharField(_("Geboorteplaats ouder"), max_length=200, blank=True)
-    geboorteland_ouder = models.CharField(_("Geboorteland ouder"), max_length=4, blank=True,
+    geboorteland_ouder = models.CharField(_("Geboorteland ouder"), max_length=200, blank=True,
                                              help_text='Four digit code')
     geslachtsaanduiding_ouder = models.CharField(_("Geslachtsaanduiding ouder"), max_length=1, blank=True,
                                                  help_text='M for mannen and V for vrouwen')
     datum_ingang_familierechtelijke_betrekking_ouder = models.CharField(
-        _("Datum ingang familierechtelijke betrekking ouder"), max_length=4, blank=True, help_text='Four digit code')
+        _("Datum ingang familierechtelijke betrekking ouder"), max_length=200, blank=True, help_text='Four digit code')
     registergemeente_akte_waaraan_gegevens_over_ouder_ontleend_zijn = models.CharField(
-        _("Registergemeente akte waaraan gegevens over ouder ontleend zijn"), max_length=4, blank=True,
+        _("Registergemeente akte waaraan gegevens over ouder ontleend zijn"), max_length=200, blank=True,
         help_text='Four digit code')
     aktenummer_van_de_akte_waaraan_gegevens = models.CharField(
         _("Aktenummer van de akte waaraan gegevens over ouder ontleend zijn"), max_length=200, blank=True)
     gemeente_waar_de_gegevens_over_ouder = models.CharField(
-        _("Gemeente waar de gegevens over ouder aan het document ontleend zijn"), max_length=4, blank=True,
+        _("Gemeente waar de gegevens over ouder aan het document ontleend zijn"), max_length=200, blank=True,
         help_text='Four digit code')
     datum_van_de_ontlening_van_de_gegevens_over_ouder = models.CharField(
         _("Datum van de ontlening van de gegevens over ouder"), max_length=200, blank=True)
@@ -83,24 +83,25 @@ class Ouder(models.Model):
         _("Beschrijving van het document waaraan de gegevens over ouder ontleend zijn"), max_length=200, blank=True)
     aanduiding_gegevens_in_onderzoek = models.CharField(_("Aanduiding gegevens in onderzoek"), max_length=200,
                                                         blank=True)
-    datum_ingang_onderzoek = models.CharField(_("Datum ingang onderzoek"), max_length=8, help_text='Format YYYYMMDD',
+    datum_ingang_onderzoek = models.CharField(_("Datum ingang onderzoek"), max_length=200, help_text='Format YYYYMMDD',
                                                  blank=True)
-    datum_einde_onderzoek = models.CharField(_("Datum einde onderzoek"), max_length=8, help_text='Format YYYYMMDD',
+    datum_einde_onderzoek = models.CharField(_("Datum einde onderzoek"), max_length=200, help_text='Format YYYYMMDD',
                                                 blank=True)
     indicatie_onjuist_dan_wel_strijdigheid_met_de_openbare_orde = models.CharField(
         _("Indicatie onjuist dan wel strijdigheid met de openbare orde"), max_length=1, blank=True,
         help_text='One digit code')
     ingangsdatum_geldigheid_met_betrekking = models.CharField(
-        _("Ingangsdatum geldigheid met betrekking tot de elementen van de categorie Ouder"), max_length=8,
+        _("Ingangsdatum geldigheid met betrekking tot de elementen van de categorie Ouder"), max_length=200,
         help_text='Format YYYYMMDD',
         blank=True)
     datum_van_opneming_met_betrekking = models.CharField(
-        _("Datum van opneming met betrekking tot de elementen van de categorie Ouder"), max_length=8,
+        _("Datum van opneming met betrekking tot de elementen van de categorie Ouder"), max_length=200,
         help_text='Format YYYYMMDD', blank=True)
 
 
 class Nationaliteit(models.Model):
-    nationaliteit = models.CharField(_("Nationaliteit"), max_length=4, blank=True, help_text='Four digit code')
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
+    nationaliteit = models.CharField(_("Nationaliteit"), max_length=200, blank=True, help_text='Four digit code')
     reden_opname_nationaliteit = models.CharField(_("Reden opname nationaliteit"), max_length=3, blank=True,
                                                      help_text='Three digit code')
     reden_beeindigen_nationaliteit = models.CharField(_("Reden beëindigen nationaliteit"), max_length=3, blank=True,
@@ -110,31 +111,32 @@ class Nationaliteit(models.Model):
     eu_persoonsummer = models.CharField(_("EU-persoonsummer"), max_length=200, blank=True)
     gemeente_waar_de_gegevens_over_nationaliteit = models.CharField(
         _("Gemeente waar de gegevens over nationaliteit aan het document ontleend dan wel afgeleid zijn"),
-        max_length=4, blank=True, help_text='Four digit code')
+        max_length=200, blank=True, help_text='Four digit code')
     datum_van_de_ontlening = models.CharField(
-        _("Datum van de ontlening dan wel afleiding van de gegevens over nationaliteit"), max_length=8, blank=True,
+        _("Datum van de ontlening dan wel afleiding van de gegevens over nationaliteit"), max_length=200, blank=True,
         help_text='Format YYYYMMDD')
     beschrijving_van_het_document = models.CharField(
         _("Beschrijving van het document waaraan de gegevens over nationaliteit ontleend dan wel afgeleid zijn"),
         max_length=200, blank=True)
     aanduiding_gegevens_in_onderzoek = models.CharField(_("Aanduiding gegevens in onderzoek"), max_length=200,
                                                         blank=True)
-    datum_ingang_onderzoek = models.CharField(_("Datum ingang onderzoek"), max_length=8, blank=True,
+    datum_ingang_onderzoek = models.CharField(_("Datum ingang onderzoek"), max_length=200, blank=True,
                                                  help_text='Format YYYYMMDD')
-    datum_einde_onderzoek = models.CharField(_("Datum einde onderzoek"), max_length=8, blank=True,
+    datum_einde_onderzoek = models.CharField(_("Datum einde onderzoek"), max_length=200, blank=True,
                                                  help_text='Format YYYYMMDD')
     indicatie_onjuist = models.CharField(_("Indicatie onjuist"), max_length=200, blank=True)
     datum_van_ingang_geldigheid_met_betrekking = models.CharField(
-        _("Datum van ingang geldigheid met betrekking tot de elementen van de categorie Nationaliteit"), max_length=8,
+        _("Datum van ingang geldigheid met betrekking tot de elementen van de categorie Nationaliteit"), max_length=200,
         blank=True, help_text='Format YYYYMMDD')
     datum_van_opneming_met_betrekking = models.CharField(
-        _("Datum van opneming met betrekking tot de elementen van de categorie Nationaliteit"), max_length=8,
+        _("Datum van opneming met betrekking tot de elementen van de categorie Nationaliteit"), max_length=200,
         blank=True, help_text='Format YYYYMMDD')
     rni_deelnemer = models.CharField(_("RNI-deelnemer"), max_length=200, blank=True)
     omschrijving_verdrag = models.CharField(_("Omschrijving verdrag"), max_length=200, blank=True)
 
 
 class Partnerschap(models.Model):
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
     a_nummer_echtgenoot_geregistreerd_partner = models.CharField(_("A-nummer echtgenoot/geregistreerd partner"),
                                                                      max_length=200, blank=True)
     burgerservicenummer_echtgenoot_geregistreerd_partner = models.CharField(
@@ -200,6 +202,7 @@ class Partnerschap(models.Model):
 
 
 class Overlijden(models.Model):
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
     datum_overlijden = models.CharField(_("Datum overlijden"), max_length=200, blank=True)
     plaats_overlijden = models.CharField(_("Plaats overlijden"), max_length=200, blank=True)
     land_overlijden = models.CharField(_("Land overlijden"), max_length=200, blank=True)
@@ -229,6 +232,7 @@ class Overlijden(models.Model):
 
 
 class Inschrijving(models.Model):
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
     datum_ingang_blokkering_pl = models.CharField(_("Datum ingang blokkering PL"), max_length=200, blank=True)
     datum_opschorting_bijhouding = models.CharField(_("Datum opschorting bijhouding"), max_length=200, blank=True)
     omschrijving_reden_opschorting_bijhouding = models.CharField(_("Omschrijving reden opschorting bijhouding"),
@@ -249,6 +253,7 @@ class Inschrijving(models.Model):
 
 
 class Verblijfplaats(models.Model):
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
     gemeente_van_inschrijving = models.CharField(_("Gemeente van inschrijving"), max_length=200, blank=True)
     datum_inschrijving_in_de_gemeente = models.CharField(_("Datum inschrijving in de gemeente"), max_length=200,
                                                          blank=True)
@@ -294,6 +299,7 @@ class Verblijfplaats(models.Model):
 
 
 class Kind(models.Model):
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
     a_nummer_kind = models.CharField(_("A-nummer kind"), max_length=200, blank=True)
     burgerservicenummer_kind = models.CharField(_("Burgerservicenummer kind"), max_length=200, blank=True)
     voornamen_kind = models.CharField(_("Voornamen kind"), max_length=200, blank=True)
@@ -327,6 +333,7 @@ class Kind(models.Model):
 
 
 class Verblijfstitel(models.Model):
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
     aanduiding_verblijfstitel = models.CharField(_("Aanduiding verblijfstitel"), max_length=200, blank=True)
     datum_einde_verblijfstitel = models.CharField(_("Datum einde verblijfstitel"), max_length=200, blank=True)
     ingangsdatum_verblijfstitel = models.CharField(_("Ingangsdatum verblijfstitel"), max_length=200, blank=True)
@@ -344,6 +351,7 @@ class Verblijfstitel(models.Model):
 
 
 class Gezagsverhouding(models.Model):
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
     indicatie_gezag_minderjarige = models.CharField(_("Indicatie gezag minderjarige"), max_length=200, blank=True)
     indicatie_curateleregister = models.CharField(_("Indicatie curateleregister"), max_length=200, blank=True)
     gemeente_waar_de_gegevens_over_gezagsverhouding = models.CharField(
@@ -367,6 +375,7 @@ class Gezagsverhouding(models.Model):
 
 
 class Reisdocument(models.Model):
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
     soort_nederlands_reisdocument = models.CharField(_("Soort Nederlands reisdocument"), max_length=200, blank=True)
     nummer_nederlands_reisdocument = models.CharField(_("Nummer Nederlands reisdocument"), max_length=200, blank=True)
     datum_uitgifte_nederlands_reisdocument = models.CharField(_("Datum uitgifte Nederlands reisdocument"),
@@ -400,6 +409,7 @@ class Reisdocument(models.Model):
 
 
 class Kiesrecht(models.Model):
+    persoon = models.ForeignKey(Persoon, null=True, on_delete=models.CASCADE)
     aanduiding_europees_kiesrecht = models.CharField(_("Aanduiding Europees kiesrecht"), max_length=200, blank=True)
     datum_verzoek_of_mededeling_europees_kiesrecht = models.CharField(
         _("Datum verzoek of mededeling Europees kiesrecht"), max_length=200, blank=True)
