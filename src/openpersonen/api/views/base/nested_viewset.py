dba9e14fb2d398f416c4fb204c7018b08dba4e4c
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-09-08 14:10:50 +0200

    :construction: Implement first list endpoint

--- src/openpersonen/api/views/base/nested_viewset.py
+++ src/openpersonen/api/views/base/nested_viewset.py
@@ -6,7 +6,13 @@ from openpersonen.api.views.base import BaseViewSet
 
 class NestedViewSet(BaseViewSet):
     def list(self, request, *args, **kwargs):
-        return Response(data=[], status=HTTP_200_OK)
+        bsn = kwargs["ingeschrevenpersonen_burgerservicenummer"]
+
+        instances = self.instance_class.list(bsn)
+
+        serializer = self.serializer_class(instances, many=True)
+
+        return Response(data=serializer.data, status=HTTP_200_OK)
 
     def retrieve(self, request, *args, **kwargs):
         bsn = kwargs["ingeschrevenpersonen_burgerservicenummer"]
