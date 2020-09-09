3fbf8be814b2286c7198624171244aed3c91e8d0
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-09-09 15:53:51 +0200

    :construction: Catch object does not exist error and return a 404

---
+++ src/openpersonen/api/views/generic_responses/default_404.py
@@ -0,0 +1,8 @@
+response_data_404 = {
+    "type": "https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#/10.4.5 404 Not Found",
+    "title": "Opgevraagde resource bestaat niet.",
+    "status": 404,
+    "detail": "The server has not found anything matching the Request-URI.",
+    "instance": "https://datapunt.voorbeeldgemeente.nl/api/v1/resourcenaam?parameter=waarde",
+    "code": "notFound"
+}
