dd27121a3305937ca5042a2eb2b07e7aa5413a26
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-09-17 14:12:02 +0200

    :sparkles: Add factory models for testing

---
+++ src/openpersonen/api/tests/factory_models/inschrijving.py
@@ -0,0 +1,25 @@
+import factory
+
+from openpersonen.api.demo_models import Inschrijving
+from .persoon import PersoonFactory
+
+
+class InschrijvingFactory(factory.django.DjangoModelFactory):
+
+    persoon = factory.SubFactory(PersoonFactory)
+    datum_ingang_blokkering_pl = 20150614
+    datum_opschorting_bijhouding = 20170912
+    omschrijving_reden_opschorting_bijhouding = 'E'
+    datum_eerste_inschrijving_gba_rni = 20160825
+    gemeente_waar_de_pk_zich_bevindt = 762
+    indicatie_geheim = 0
+    datum_verfificatie = 20190909
+    omschrijving_verificatie = 'Correctie adres'
+    versienummer = 5
+    datumtijdstempel = 20160901132009000
+    pk_gegevens_volledig_meegeconverteerd = 'P'
+    rni_deelnemer = 401
+    omschrijving_verdrag = ''
+
+    class Meta:
+        model = Inschrijving
---
+++ src/openpersonen/api/tests/factory_models/nationaliteit.py
@@ -0,0 +1,28 @@
+import factory
+
+from openpersonen.api.demo_models import Nationaliteit
+from .persoon import PersoonFactory
+
+
+class NationaliteitFactory(factory.django.DjangoModelFactory):
+
+    persoon = factory.SubFactory(PersoonFactory)
+    nationaliteit = 1
+    reden_opname_nationaliteit = 1
+    reden_beeindigen_nationaliteit = 401
+    aanduiding_bijzonder_nederlanderschap = 'V'
+    eu_persoonsummer = 90120113511
+    gemeente_waar_de_gegevens_over_nationaliteit = 599
+    datum_van_de_ontlening = 19940310
+    beschrijving_van_het_document = 'PK'
+    aanduiding_gegevens_in_onderzoek = 40000
+    datum_ingang_onderzoek = 20100822
+    datum_einde_onderzoek = 20200923
+    indicatie_onjuist = 'O'
+    datum_van_ingang_geldigheid_met_betrekking = 19580706
+    datum_van_opneming_met_betrekking = 19940930
+    rni_deelnemer = 201
+    omschrijving_verdrag = ''
+
+    class Meta:
+        model = Nationaliteit
