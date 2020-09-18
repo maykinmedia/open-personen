eed41e0486a8ed5d95200124d402085c11000bc6
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-09-18 10:42:22 +0200

    :white_check_mark: Add unit tests for new util function

--- src/openpersonen/api/tests/test_utils.py
+++ src/openpersonen/api/tests/test_utils.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 
 from django.test import TestCase
 
-from openpersonen.api.utils import convert_empty_instances
+from openpersonen.api.utils import convert_empty_instances, is_expected_date_format
 
 
 class TestConvertEmptyInstances(TestCase):
@@ -23,3 +23,16 @@ class TestConvertEmptyInstances(TestCase):
         convert_empty_instances(original_dict)
 
         self.assertEqual(original_dict, expected_dict)
+
+
+class TestIsExpectedDateFormat(TestCase):
+
+    def test_is_expected_date_format_returns_true_with_correct_date_format(self):
+        date = '19801223'
+        result = is_expected_date_format(date)
+        self.assertTrue(result)
+
+    def test_is_expected_date_format_returns_false_with_incorrect_date_format(self):
+        date = '19800000'
+        result = is_expected_date_format(date)
+        self.assertFalse(result)
