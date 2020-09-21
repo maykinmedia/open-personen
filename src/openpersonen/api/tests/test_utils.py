eaa52f82d7ee8e3d90eccdf7d0527e4bd7fc417b
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-09-21 12:15:57 +0200

    :construction: Calculate age in different cases

--- src/openpersonen/api/tests/test_utils.py
+++ src/openpersonen/api/tests/test_utils.py
@@ -1,8 +1,10 @@
 from collections import OrderedDict
 
+from freezegun import freeze_time
+
 from django.test import TestCase
 
-from openpersonen.api.utils import convert_empty_instances
+from openpersonen.api.utils import convert_empty_instances, calculate_age
 
 
 class TestConvertEmptyInstances(TestCase):
@@ -23,3 +25,24 @@ class TestConvertEmptyInstances(TestCase):
         convert_empty_instances(original_dict)
 
         self.assertEqual(original_dict, expected_dict)
+
+
+class TestCalculateAge(TestCase):
+
+    @freeze_time("2020-09-12")
+    def test_calculate_age_with_full_date(self):
+        birth_date = '19750101'
+        result = calculate_age(birth_date)
+        self.assertEqual(result, 45)
+
+    @freeze_time("2020-09-12")
+    def test_calculate_age_with_just_year(self):
+        birth_date = '19610000'
+        result = calculate_age(birth_date)
+        self.assertEqual(result, 59)
+
+    @freeze_time("2020-09-12")
+    def test_calculate_age_with_no_data(self):
+        birth_date = ''
+        result = calculate_age(birth_date)
+        self.assertEqual(result, 0)
