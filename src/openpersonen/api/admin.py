cd262c6403b51d4d8f13ef91b789b83bbe30032e
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-09-29 09:41:43 +0200

    :construction: Add some demo models to the admin screen

--- src/openpersonen/api/admin.py
+++ src/openpersonen/api/admin.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from solo.admin import SingletonModelAdmin
 
 from .models import StufBGClient
+from .demo_models import Persoon, Kind, Ouder
 
 
 @admin.register(StufBGClient)
@@ -52,3 +53,96 @@ class StufBGClientAdmin(SingletonModelAdmin):
             },
         ),
     )
+
+
+class KindInline(admin.TabularInline):
+    model = Kind
+    extra = 0
+    fields = (
+        'burgerservicenummer_kind',
+        'voornamen_kind',
+        'geslachtsnaam_kind',
+        'geboortedatum_kind'
+    )
+
+
+class OuderInline(admin.TabularInline):
+    model = Ouder
+    extra = 0
+    fields = (
+        'burgerservicenummer_ouder',
+        'voornamen_ouder',
+        'geslachtsnaam_ouder',
+        'geboortedatum_ouder'
+    )
+
+
+@admin.register(Persoon)
+class PersoonAdmin(admin.ModelAdmin):
+    list_display = (
+        "burgerservicenummer_persoon",
+        "voornamen_persoon",
+        "geslachtsnaam_persoon",
+    )
+
+    inlines = [
+        KindInline,
+        OuderInline
+    ]
+
+    fieldsets = (
+        (
+            _("Info"),
+            {
+                "fields": (
+                    "burgerservicenummer_persoon",
+                    "voornamen_persoon",
+                    "geslachtsnaam_persoon",
+                    "geboortedatum_persoon",
+                    "geslachtsaanduiding",
+                )
+            },
+        ),
+    )
+
+    def save_formset(self, request, form, formset, change):
+        for form in formset.forms:
+            instance = form.instance
+            if instance.pk is None and isinstance(instance, Kind):
+                instance.save()
+                persoon_instance, is_new = Persoon.objects.get_or_create(
+                    burgerservicenummer_persoon=instance.burgerservicenummer_kind
+                )
+                if is_new:
+                    persoon_instance.voornamen_persoon = instance.voornamen_kind
+                    persoon_instance.geslachtsnaam_persoon = instance.geslachtsnaam_kind
+                    persoon_instance.geboortedatum_persoon = instance.geboortedatum_kind
+                    persoon_instance.save()
+
+                Ouder.objects.create(
+                    persoon=persoon_instance,
+                    burgerservicenummer_ouder=instance.persoon.burgerservicenummer_persoon,
+                    voornamen_ouder=instance.persoon.voornamen_persoon,
+                    geslachtsnaam_ouder=instance.persoon.geslachtsnaam_persoon,
+                    geboortedatum_ouder=instance.persoon.geboortedatum_persoon,
+                )
+            if instance.pk is None and isinstance(instance, Ouder):
+                instance.save()
+                persoon_instance, is_new = Persoon.objects.get_or_create(
+                    burgerservicenummer_persoon=instance.burgerservicenummer_ouder
+                )
+                if is_new:
+                    persoon_instance.voornamen_persoon = instance.voornamen_ouder
+                    persoon_instance.geslachtsnaam_persoon = instance.geslachtsnaam_ouder
+                    persoon_instance.geboortedatum_persoon = instance.geboortedatum_ouder
+                    persoon_instance.save()
+
+                Kind.objects.create(
+                    persoon=persoon_instance,
+                    burgerservicenummer_kind=instance.persoon.burgerservicenummer_persoon,
+                    voornamen_kind=instance.persoon.voornamen_persoon,
+                    geslachtsnaam_kind=instance.persoon.geslachtsnaam_persoon,
+                    geboortedatum_kind=instance.persoon.geboortedatum_persoon,
+                )
+
+        formset.save()
