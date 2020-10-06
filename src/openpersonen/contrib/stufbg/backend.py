80bb7cb69aff86410051d81938d07036ade30cd6
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-10-06 12:20:46 +0200

    :construction: Set up stufbg Backend class

--- src/openpersonen/contrib/stufbg/backend.py
+++ src/openpersonen/contrib/stufbg/backend.py
@@ -1,24 +1,26 @@
+from django.apps import apps
+
+
 class BackEnd:
     def get_person(self, bsn=None, filters=None):
         if not bsn and not filters:
             raise ValueError("Either bsn or filters must be supplied")
 
-        print("In get_person")
+        StufBGClient = apps.get_model("stufbg", "StufBGClient")
 
-        return None
+        if bsn:
+            return StufBGClient.get_solo().get_ingeschreven_persoon(bsn=bsn)
+        else:
+            return StufBGClient.get_solo().get_ingeschreven_persoon(filters=filters)
 
-    def get_kind(self, bsn, kind_bsn=None):
+    def get_kind(self, bsn, **kwargs):
+        StufBGClient = apps.get_model("stufbg", "StufBGClient")
+        return StufBGClient.get_solo().get_kind(bsn)
 
-        print("In get_kind")
+    def get_ouder(self, bsn, **kwargs):
+        StufBGClient = apps.get_model("stufbg", "StufBGClient")
+        return StufBGClient.get_solo().get_ouder(bsn)
 
-        return None
-
-    def get_ouder(self, bsn, ouder_bsn=None):
-        print("In get_ouder")
-
-        return None
-
-    def get_partner(self, bsn, partner_bsn=None):
-        print("In get_partner")
-
-        return None
+    def get_partner(self, bsn, **kwargs):
+        StufBGClient = apps.get_model("stufbg", "StufBGClient")
+        return StufBGClient.get_solo().get_partner(bsn)
