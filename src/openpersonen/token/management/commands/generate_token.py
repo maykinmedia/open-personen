69c92cf4bc6ba4fbaf43cf4a8383275865de667c
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-02 12:37:19 +0100

    :sparkles: Add username argument to be passed in when creating a token

--- src/openpersonen/token/management/commands/generate_token.py
+++ src/openpersonen/token/management/commands/generate_token.py
@@ -1,5 +1,3 @@
-import uuid
-
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
@@ -12,6 +10,9 @@ User = get_user_model()
 class Command(BaseCommand):
     help = 'Create an API Token'
 
+    def add_arguments(self, parser):
+        parser.add_argument('username', type=str)
+
     def handle(self, *args, **options):
-        user = User.objects.create(username=uuid.uuid4())
+        user = User.objects.create(username=options['username'])
         self.stdout.write(f'Generated token: {Token.objects.create(user=user).key}')
