222c88b2a02cac05d5c56494a1c3c3f8557f6a42
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-02 13:54:44 +0100

    :art: Style code with black and isort

--- src/openpersonen/token/management/commands/generate_token.py
+++ src/openpersonen/token/management/commands/generate_token.py
@@ -3,17 +3,16 @@ from django.core.management.base import BaseCommand
 
 from rest_framework.authtoken.models import Token
 
-
 User = get_user_model()
 
 
 class Command(BaseCommand):
-    help = 'Create an API Token'
+    help = "Create an API Token"
 
     def add_arguments(self, parser):
-        parser.add_argument('username', type=str)
+        parser.add_argument("username", type=str)
 
     def handle(self, *args, **options):
-        user, _ = User.objects.get_or_create(username=options['username'])
+        user, _ = User.objects.get_or_create(username=options["username"])
         Token.objects.filter(user=user).delete()
-        self.stdout.write(f'Generated token: {Token.objects.create(user=user).key}')
+        self.stdout.write(f"Generated token: {Token.objects.create(user=user).key}")
