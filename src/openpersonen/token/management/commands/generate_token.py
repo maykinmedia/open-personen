048bf6cb2cdb058d76c8d677336ce314792227fd
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-12 09:42:23 +0100

    :art: Style code

--- src/openpersonen/token/management/commands/generate_token.py
+++ src/openpersonen/token/management/commands/generate_token.py
@@ -11,21 +11,27 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--noinput', '--no-input', action='store_false', dest='interactive',
+            "--noinput",
+            "--no-input",
+            action="store_false",
+            dest="interactive",
             help="Do NOT prompt the user for input of any kind.",
         )
 
         parser.add_argument("username", type=str)
 
     def handle(self, *args, **options):
-        interactive = options['interactive']
+        interactive = options["interactive"]
         username = options["username"]
         user, _ = User.objects.get_or_create(username=username)
 
         user_has_token = Token.objects.filter(user=user).exists()
 
-        if user_has_token and interactive and \
-            input(f'Replace existing token for {username}? [yes/no] ') != 'yes':
+        if (
+            user_has_token
+            and interactive
+            and input(f"Replace existing token for {username}? [yes/no] ") != "yes"
+        ):
             token = Token.objects.get(user=user)
         else:
             Token.objects.filter(user=user).delete()
