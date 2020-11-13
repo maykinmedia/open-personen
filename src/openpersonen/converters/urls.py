25414501985dbff57539bf842e69c030de07c714
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-13 10:13:40 +0100

    :art: Style code

--- src/openpersonen/converters/urls.py
+++ src/openpersonen/converters/urls.py
@@ -1,6 +1,18 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path("api2stufbg/", include(("openpersonen.converters.api2stufbg.urls", "openpersonen.converters"), namespace='api2stufbg')),
-    path("stufbg2api/", include(("openpersonen.converters.stufbg2api.urls", "openpersonen.converters"), namespace='stufbg2api')),
+    path(
+        "api2stufbg/",
+        include(
+            ("openpersonen.converters.api2stufbg.urls", "openpersonen.converters"),
+            namespace="api2stufbg",
+        ),
+    ),
+    path(
+        "stufbg2api/",
+        include(
+            ("openpersonen.converters.stufbg2api.urls", "openpersonen.converters"),
+            namespace="stufbg2api",
+        ),
+    ),
 ]
