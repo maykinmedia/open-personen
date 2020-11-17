70e9aab54c05886d1574aebc76999b779f6017b7
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-17 15:13:39 +0100

    :white_check_mark: Update unit tests for new endpoints

--- src/openpersonen/converters/api2stufbg/tests.py
+++ src/openpersonen/converters/api2stufbg/tests.py
@@ -1,27 +1,83 @@
 from django.urls import reverse
 
+from freezegun import freeze_time
+from mock import patch
 from rest_framework.test import APITestCase
+
+from openpersonen.contrib.stufbg.models import StufBGClient
 
 
 class TestPersoonView(APITestCase):
-    def test_persoon_view(self):
-        response = self.client.post(reverse("api2stufbg:persoon"))
+    @freeze_time("2020-11-17")
+    @patch("uuid.uuid4")
+    def test_persoon_view(self, uuid_mock):
+        uuid_mock.return_value = "00000000-0000-0000-0000-000000000000"
+        bsn = 123456789
+        expected_data = StufBGClient.get_solo().get_persoon_request_data(
+            filters={"burgerservicenummer": str(bsn)}
+        )
+
+        response = self.client.get(
+            reverse("api2stufbg:ingeschrevenpersonen-list")
+            + f"?burgerservicenummer={bsn}"
+        )
+
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, expected_data)
 
 
 class TestOuderView(APITestCase):
-    def test_ouder_view(self):
-        response = self.client.post(reverse("api2stufbg:ouder"))
+    @freeze_time("2020-11-17")
+    @patch("uuid.uuid4")
+    def test_ouder_view(self, uuid_mock):
+        uuid_mock.return_value = "00000000-0000-0000-0000-000000000000"
+        bsn = 123456789
+        expected_data = StufBGClient.get_solo().get_nested_request_data(
+            "request/RequestOuder.xml", bsn
+        )
+        response = self.client.get(
+            reverse(
+                "api2stufbg:ouders-list",
+                kwargs={"ingeschrevenpersonen_burgerservicenummer": bsn},
+            )
+        )
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, expected_data)
 
 
 class TestKindView(APITestCase):
-    def test_kind_view(self):
-        response = self.client.post(reverse("api2stufbg:kind"))
+    @freeze_time("2020-11-17")
+    @patch("uuid.uuid4")
+    def test_kind_view(self, uuid_mock):
+        uuid_mock.return_value = "00000000-0000-0000-0000-000000000000"
+        bsn = 123456789
+        expected_data = StufBGClient.get_solo().get_nested_request_data(
+            "request/RequestKind.xml", bsn
+        )
+        response = self.client.get(
+            reverse(
+                "api2stufbg:kinderen-list",
+                kwargs={"ingeschrevenpersonen_burgerservicenummer": bsn},
+            )
+        )
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, expected_data)
 
 
 class TestPartnerView(APITestCase):
-    def test_partner_view(self):
-        response = self.client.post(reverse("api2stufbg:partner"))
+    @freeze_time("2020-11-17")
+    @patch("uuid.uuid4")
+    def test_partner_view(self, uuid_mock):
+        uuid_mock.return_value = "00000000-0000-0000-0000-000000000000"
+        bsn = 123456789
+        expected_data = StufBGClient.get_solo().get_nested_request_data(
+            "request/RequestPartner.xml", bsn
+        )
+        response = self.client.get(
+            reverse(
+                "api2stufbg:partners-list",
+                kwargs={"ingeschrevenpersonen_burgerservicenummer": bsn},
+            )
+        )
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, expected_data)
