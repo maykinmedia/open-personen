d04f8255600bb5b73b39f898b6392ae8a68b1a52
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-18 10:22:41 +0100

    :sparkles: Implement converting soap ui response to api json response

--- src/openpersonen/converters/stufbg2api/views.py
+++ src/openpersonen/converters/stufbg2api/views.py
@@ -3,13 +3,21 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from openpersonen.utils.instance_dicts import (
+    convert_xml_to_kind_dict,
+    convert_xml_to_ouder_dict,
+    convert_xml_to_partner_dict,
+    convert_xml_to_persoon_dicts,
+)
+
 
 class PersoonView(APIView):
 
     renderer_classes = [JSONRenderer]
 
     def post(self, request):
-        return Response(status=status.HTTP_200_OK)
+        data = convert_xml_to_persoon_dicts(request.body)
+        return Response(data=data, status=status.HTTP_200_OK)
 
 
 class OuderView(APIView):
@@ -17,7 +25,8 @@ class OuderView(APIView):
     renderer_classes = [JSONRenderer]
 
     def post(self, request):
-        return Response(status=status.HTTP_200_OK)
+        data = convert_xml_to_ouder_dict(request.body)
+        return Response(data=data, status=status.HTTP_200_OK)
 
 
 class KindView(APIView):
@@ -25,7 +34,8 @@ class KindView(APIView):
     renderer_classes = [JSONRenderer]
 
     def post(self, request):
-        return Response(status=status.HTTP_200_OK)
+        data = convert_xml_to_kind_dict(request.body)
+        return Response(data=data, status=status.HTTP_200_OK)
 
 
 class PartnerView(APIView):
@@ -33,4 +43,5 @@ class PartnerView(APIView):
     renderer_classes = [JSONRenderer]
 
     def post(self, request):
-        return Response(status=status.HTTP_200_OK)
+        data = convert_xml_to_partner_dict(request.body)
+        return Response(data=data, status=status.HTTP_200_OK)
