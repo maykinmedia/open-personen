af9e596bd245daf1e7cc3e5802a8fe86e4b5da83
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-25 16:35:54 +0100

    :white_check_mark: Move code into separate functions

--- src/openpersonen/features/aanhef.py
+++ src/openpersonen/features/aanhef.py
@@ -33,6 +33,50 @@ def get_salutation_from_gender_designation(gender_designation):
         return "Geachte heer"
 
 
+def get_aanhef_last_name(
+    last_name_prefix,
+    last_name,
+    partner_last_name_prefix,
+    partner_last_name,
+    indication_name_use,
+):
+    aanhef_last_name = ""
+    if indication_name_use == "E":  # Eigen
+        if last_name_prefix:
+            aanhef_last_name += f" {last_name_prefix.capitalize()}"
+        if last_name:
+            aanhef_last_name += f" {last_name}"
+    if indication_name_use == "N":  # Partner na eigen
+        if last_name_prefix:
+            aanhef_last_name += f" {last_name_prefix.capitalize()}"
+        if last_name:
+            aanhef_last_name += f" {last_name}-"
+        if partner_last_name_prefix:
+            aanhef_last_name += f"{partner_last_name_prefix} "
+        if partner_last_name:
+            aanhef_last_name += f"{partner_last_name}"
+    if indication_name_use == "P":  # Partner
+        if partner_last_name_prefix:
+            if not partner_last_name_prefix[0].isupper():
+                partner_last_name_prefix = partner_last_name_prefix.capitalize()
+            aanhef_last_name += f" {partner_last_name_prefix}"
+        if partner_last_name:
+            aanhef_last_name += f" {partner_last_name}"
+    if indication_name_use == "V":  # Partner voor eigen
+        if partner_last_name_prefix:
+            aanhef_last_name += f" {partner_last_name_prefix.capitalize()}"
+        if partner_last_name:
+            aanhef_last_name += f" {partner_last_name}-"
+        else:
+            aanhef_last_name += f" "
+        if last_name_prefix:
+            aanhef_last_name += f"{last_name_prefix} "
+        if last_name:
+            aanhef_last_name += f"{last_name}"
+
+    return aanhef_last_name
+
+
 def get_aanhef(
     last_name_prefix,
     last_name,
@@ -43,14 +87,13 @@ def get_aanhef(
     title,
     partner_title,
 ):
-
-    if indication_name_use == "P" and title != "Jonkheer":
-        pass
-    elif title == "Jonkvrouw" and (partner_last_name_prefix or partner_last_name):
-        pass
-    elif title == "Jonkvrouw" and indication_name_use != "E":
-        pass
-    else:
+    if (
+        not (indication_name_use == "P" and title != "Jonkheer")
+        and not (
+            title == "Jonkvrouw" and (partner_last_name_prefix or partner_last_name)
+        )
+        and not (title == "Jonkvrouw" and indication_name_use != "E")
+    ):
         salutation = get_salutation_from_title(title)
         if salutation:
             return salutation
@@ -62,38 +105,13 @@ def get_aanhef(
 
     salutation = get_salutation_from_gender_designation(gender_designation)
     if salutation:
-        if indication_name_use == "E":  # Eigen
-            if last_name_prefix:
-                salutation += f" {last_name_prefix.capitalize()}"
-            if last_name:
-                salutation += f" {last_name}"
-        if indication_name_use == "N":  # Partner na eigen
-            if last_name_prefix:
-                salutation += f" {last_name_prefix.capitalize()}"
-            if last_name:
-                salutation += f" {last_name}-"
-            if partner_last_name_prefix:
-                salutation += f"{partner_last_name_prefix} "
-            if partner_last_name:
-                salutation += f"{partner_last_name}"
-        if indication_name_use == "P":  # Partner
-            if partner_last_name_prefix:
-                if not partner_last_name_prefix[0].isupper():
-                    partner_last_name_prefix = partner_last_name_prefix.capitalize()
-                salutation += f" {partner_last_name_prefix}"
-            if partner_last_name:
-                salutation += f" {partner_last_name}"
-        if indication_name_use == "V":  # Partner voor eigen
-            if partner_last_name_prefix:
-                salutation += f" {partner_last_name_prefix.capitalize()}"
-            if partner_last_name:
-                salutation += f" {partner_last_name}-"
-            else:
-                salutation += f" "
-            if last_name_prefix:
-                salutation += f"{last_name_prefix} "
-            if last_name:
-                salutation += f"{last_name}"
+        salutation += get_aanhef_last_name(
+            last_name_prefix,
+            last_name,
+            partner_last_name_prefix,
+            partner_last_name,
+            indication_name_use,
+        )
 
         return salutation
 
