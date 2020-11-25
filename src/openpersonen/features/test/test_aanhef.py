8230a358eaf1f7c6dd1d753a694620c7f4caa70b
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-25 09:33:48 +0100

    :white_check_mark: Add unit test for testing aanhef with capitalized or not capitalized last names

--- src/openpersonen/features/test/test_aanhef.py
+++ src/openpersonen/features/test/test_aanhef.py
@@ -120,15 +120,107 @@ class TestGetAanHefWithoutTitle(TestCase):
         Testing examples given here: https://github.com/VNG-Realisatie/Haal-Centraal-BRP-bevragen/blob/v1.0.0/features/aanhef.feature
         """
         table_string = """
-        | aanduidingNaamgebruik | geslachtsaanduiding |samenstelling aanhef | aanschrijfwijze           | aanhef                                 |
-        | Eigen                 | Man                 | GA VV GN            | H. in het Veld            | Geachte heer In het Veld               |
-        | Eigen                 | Man                 | GA VV GN            | F. Groenen                | Geachte heer Groenen                   |
-        | Partner na eigen      | Vrouw               | GA VV GN-VP GP      | I. van Velzen-in het Veld | Geachte mevrouw Van Velzen-in het Veld |
-        | Partner na eigen      | Vrouw               | GA VV GN-VP GP      | F. Groenen-Groenink       | Geachte mevrouw Groenen-Groenink       |
-        | Partner               | Vrouw               | GA VP GP            | S. van Velzen             | Geachte mevrouw Van Velzen             |
-        | Partner               | Vrouw               | GA VP GP            | J.F.R. Groenen            | Geachte mevrouw Groenen                |
-        | Partner voor eigen    | Man                 | GA VP GP-VV GN      | F. in het Veld-van Velzen | Geachte heer In het Veld-van Velzen    |
-        | Partner voor eigen    | Man                 | GA VP GP-VV GN      | F. Groenen-Groenink       | Geachte heer Groenen-Groenink          |
+            | aanduidingNaamgebruik | geslachtsaanduiding |samenstelling aanhef | aanschrijfwijze           | aanhef                                 |
+            | Eigen                 | Man                 | GA VV GN            | H. in het Veld            | Geachte heer In het Veld               |
+            | Eigen                 | Man                 | GA VV GN            | F. Groenen                | Geachte heer Groenen                   |
+            | Partner na eigen      | Vrouw               | GA VV GN-VP GP      | I. van Velzen-in het Veld | Geachte mevrouw Van Velzen-in het Veld |
+            | Partner na eigen      | Vrouw               | GA VV GN-VP GP      | F. Groenen-Groenink       | Geachte mevrouw Groenen-Groenink       |
+            | Partner               | Vrouw               | GA VP GP            | S. van Velzen             | Geachte mevrouw Van Velzen             |
+            | Partner               | Vrouw               | GA VP GP            | J.F.R. Groenen            | Geachte mevrouw Groenen                |
+            | Partner voor eigen    | Man                 | GA VP GP-VV GN      | F. in het Veld-van Velzen | Geachte heer In het Veld-van Velzen    |
+            | Partner voor eigen    | Man                 | GA VP GP-VV GN      | F. Groenen-Groenink       | Geachte heer Groenen-Groenink          |
+        """
+
+        aanduiding_naamgebruik_to_enumeration = {
+            "Eigen": "E",
+            "Partner na eigen": "N",
+            "Partner": "P",
+            "Partner voor eigen": "V",
+        }
+
+        geslachtsaanduiding_to_enumeration = {"Man": "M", "Vrouw": "V"}
+
+        # Convert table string to rows and remove empty rows, white spaces, and header row
+        table_rows = [
+            [item.strip() for item in row.strip().split("|") if item]
+            for row in table_string.split("\n")
+            if row.strip()
+        ][1:]
+
+        for row in table_rows:
+            (
+                aanduiding_naamgebruik,
+                geslachtsaanduiding,
+                _,
+                aanschrijfwijze,
+                aanhef,
+            ) = row
+
+            last_name_prefix = None
+            last_name = None
+            partner_last_name_prefix = None
+            partner_last_name = None
+
+            if aanduiding_naamgebruik == "Eigen":
+                last_name = aanschrijfwijze.split(" ", 1)[-1]
+                if len(last_name.split(" ")) > 1:
+                    split_last_name = last_name.split(" ")
+                    last_name_prefix = " ".join(split_last_name[:-1])
+                    last_name = split_last_name[-1]
+            elif aanduiding_naamgebruik == "Partner na eigen":
+                last_name = aanschrijfwijze.split(" ", 1)[-1]
+                last_name, partner_last_name = last_name.split("-")
+                if len(last_name.split(" ")) > 1:
+                    split_last_name = last_name.split(" ")
+                    last_name_prefix = " ".join(split_last_name[:-1])
+                    last_name = split_last_name[-1]
+                if len(partner_last_name.split(" ")) > 1:
+                    split_partner_last_name = partner_last_name.split(" ")
+                    partner_last_name_prefix = " ".join(split_partner_last_name[:-1])
+                    partner_last_name = split_partner_last_name[-1]
+            elif aanduiding_naamgebruik == "Partner":
+                partner_last_name = aanschrijfwijze.split(" ", 1)[-1]
+                if len(partner_last_name.split(" ")) > 1:
+                    split_partner_last_name = partner_last_name.split(" ")
+                    partner_last_name_prefix = " ".join(split_partner_last_name[:-1])
+                    partner_last_name = split_partner_last_name[-1]
+            elif aanduiding_naamgebruik == "Partner voor eigen":
+                last_name = aanschrijfwijze.split(" ", 1)[-1]
+                partner_last_name, last_name = last_name.split("-")
+                if len(last_name.split(" ")) > 1:
+                    split_last_name = last_name.split(" ")
+                    last_name_prefix = " ".join(split_last_name[:-1])
+                    last_name = split_last_name[-1]
+                if len(partner_last_name.split(" ")) > 1:
+                    split_partner_last_name = partner_last_name.split(" ")
+                    partner_last_name_prefix = " ".join(split_partner_last_name[:-1])
+                    partner_last_name = split_partner_last_name[-1]
+
+            result = get_aanhef(
+                last_name_prefix,
+                last_name,
+                partner_last_name_prefix,
+                partner_last_name,
+                aanduiding_naamgebruik_to_enumeration[aanduiding_naamgebruik],
+                geslachtsaanduiding_to_enumeration[geslachtsaanduiding],
+                None,
+                None,
+            )
+            self.assertEqual(result, aanhef)
+
+
+class TestGetAanHefWithCapitalOrSmallLeters(TestCase):
+    def test_get_aanhef_with_capital_or_small_letters(self):
+        table_string = """
+            | aanduidingNaamgebruik | geslachtsaanduiding |samenstelling aanhef | aanschrijfwijze           | aanhef                                 |
+            | Eigen                 | Man                 | GA VV GN            | H. in het Veld            | Geachte heer In het Veld               |
+            | Eigen                 | Man                 | GA VV GN            | F. Groenen                | Geachte heer Groenen                   |
+            | Partner na eigen      | Vrouw               | GA VV GN-VP GP      | I. van Velzen-in het Veld | Geachte mevrouw Van Velzen-in het Veld |
+            | Partner na eigen      | Vrouw               | GA VV GN-VP GP      | F. Groenen-Groenink       | Geachte mevrouw Groenen-Groenink       |
+            | Partner               | Vrouw               | GA VP GP            | S. van Velzen             | Geachte mevrouw Van Velzen             |
+            | Partner               | Vrouw               | GA VP GP            | J.F.R. Groenen            | Geachte mevrouw Groenen                |
+            | Partner voor eigen    | Man                 | GA VP GP-VV GN      | F. in het Veld-van Velzen | Geachte heer In het Veld-van Velzen    |
+            | Partner voor eigen    | Man                 | GA VP GP-VV GN      | F. Groenen-Groenink       | Geachte heer Groenen-Groenink          |
         """
 
         aanduiding_naamgebruik_to_enumeration = {
