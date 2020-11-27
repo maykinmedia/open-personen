00e79ca6cdfb020f8531f907c3baf6e8dc712464
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-27 11:25:13 +0100

    :white_check_mark: Add unit test for getting aanschrijfwijze when person has a predicate but the partner does not

--- src/openpersonen/features/aanschrijfwijze/aanschrijfwijze.py
+++ src/openpersonen/features/aanschrijfwijze/aanschrijfwijze.py
@@ -19,25 +19,45 @@ def get_aanschrijfwijze(
     partner_title,
 ):
     aanschrijfwijze = 'string'
-    if indication_name_use == 'E':
-        if last_name_prefix:
-            aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {last_name_prefix} {last_name}"
-        else:
-            aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {last_name}"
-    elif indication_name_use == 'N':
-        if last_name_prefix and partner_last_name_prefix:
-            aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {last_name_prefix} {last_name}-{partner_last_name_prefix} {partner_last_name}"
-        else:
-            aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {last_name}-{partner_last_name}"
-    elif indication_name_use == 'P':
-        if partner_last_name_prefix:
-            aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name_prefix} {partner_last_name}"
-        else:
-            aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name}"
-    elif indication_name_use == 'V':
-        if last_name_prefix and partner_last_name_prefix:
-            aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name_prefix} {partner_last_name}-{last_name_prefix} {last_name}"
-        else:
-            aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name}-{last_name}"
+
+    use_own_name = (title in ['Jonkheer', 'Jonkvrouw'] and indication_name_use != 'P')
+
+    if use_own_name:
+        if indication_name_use == 'E':
+            if last_name_prefix:
+                aanschrijfwijze = f"{title.lower()} {get_aanschrijfwijze_first_name(first_name)} {last_name_prefix} {last_name}"
+            else:
+                aanschrijfwijze = f"{title.lower()} {get_aanschrijfwijze_first_name(first_name)} {last_name}"
+        elif indication_name_use == 'N':
+            if last_name_prefix and partner_last_name_prefix:
+                aanschrijfwijze = f"{title.lower()} {get_aanschrijfwijze_first_name(first_name)} {last_name_prefix} {last_name}-{partner_last_name_prefix} {partner_last_name}"
+            else:
+                aanschrijfwijze = f"{title.lower()} {get_aanschrijfwijze_first_name(first_name)} {last_name}-{partner_last_name}"
+        elif indication_name_use == 'V':
+            if last_name_prefix and partner_last_name_prefix:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name_prefix} {partner_last_name}-{title.lower()} {last_name_prefix} {last_name}"
+            else:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name}-{title.lower()} {last_name}"
+    else:
+        if indication_name_use == 'E':
+            if last_name_prefix:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {last_name_prefix} {last_name}"
+            else:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {last_name}"
+        elif indication_name_use == 'N':
+            if last_name_prefix and partner_last_name_prefix:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {last_name_prefix} {last_name}-{partner_last_name_prefix} {partner_last_name}"
+            else:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {last_name}-{partner_last_name}"
+        elif indication_name_use == 'P':
+            if partner_last_name_prefix:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name_prefix} {partner_last_name}"
+            else:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name}"
+        elif indication_name_use == 'V':
+            if last_name_prefix and partner_last_name_prefix:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name_prefix} {partner_last_name}-{last_name_prefix} {last_name}"
+            else:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name}-{last_name}"
 
     return aanschrijfwijze
--- src/openpersonen/features/aanschrijfwijze/test/test_aanschrijfwijze.py
+++ src/openpersonen/features/aanschrijfwijze/test/test_aanschrijfwijze.py
@@ -166,3 +166,95 @@ class TestGetAanschrijfwijzePersonHasTitlePartnerDoesNot(TestCase):
             )
 
             self.assertEqual(aanschrijfwijze, result)
+
+
+class TestGetAanschrijfwijzePersonHasPredicatePartnerDoesNot(TestCase):
+    def test_aanschrijfwijze_person_has_predicate_partner_does_not(self):
+        table_string = """
+            | adellijkeTitel_predikaat | aanduidingNaamgebruik | aanschrijfwijze                    |
+            | Jonkheer                 | Eigen                 | jonkheer T. van Hoogh              |
+            | Jonkvrouw                | Eigen                 | jonkvrouw T. van Hoogh             |
+            | Jonkvrouw                | Partner na eigen      | jonkvrouw T. van Hoogh-in het Veld |
+            | Jonkvrouw                | Partner               | T. in het Veld                     |
+            | Jonkheer                 | Partner               | T. in het Veld                     |
+            | Jonkvrouw                | Partner voor eigen    | T. in het Veld-jonkvrouw van Hoogh |
+            | Jonkheer                 | Partner na eigen      | jonkheer T. van Hoogh-in het Veld  |
+        """
+
+        aanduiding_naamgebruik_to_enumeration = {
+            "Eigen": "E",
+            "Partner na eigen": "N",
+            "Partner": "P",
+            "Partner voor eigen": "V",
+        }
+
+        # Convert table string to rows and remove empty rows, white spaces, and header row
+        table_rows = [
+            [item.strip() for item in row.strip().split("|") if item]
+            for row in table_string.split("\n")
+            if row.strip()
+        ][1:]
+
+        voornamen = 'Tom'  # Example first name to use
+
+        for row in table_rows:
+            (
+                title,
+                aanduiding_naamgebruik,
+                aanschrijfwijze,
+            ) = row
+
+            name = aanschrijfwijze.replace('jonkheer ', '')
+            name = name.replace('jonkvrouw ', '')
+            last_name = None
+            last_name_prefix = None
+            partner_last_name_prefix = None
+            partner_last_name = None
+            if aanduiding_naamgebruik == "Eigen":
+                last_name = name.split(" ", 1)[-1]
+                if len(last_name.split(" ")) > 1:
+                    split_last_name = last_name.split(" ")
+                    last_name_prefix = " ".join(split_last_name[:-1])
+                    last_name = split_last_name[-1]
+            elif aanduiding_naamgebruik == "Partner na eigen":
+                last_name = name.split(" ", 1)[-1]
+                last_name, partner_last_name = last_name.split("-")
+                if len(last_name.split(" ")) > 1:
+                    split_last_name = last_name.split(" ")
+                    last_name_prefix = " ".join(split_last_name[:-1])
+                    last_name = split_last_name[-1]
+                if len(partner_last_name.split(" ")) > 1:
+                    split_partner_last_name = partner_last_name.split(" ")
+                    partner_last_name_prefix = " ".join(split_partner_last_name[:-1])
+                    partner_last_name = split_partner_last_name[-1]
+            elif aanduiding_naamgebruik == "Partner":
+                partner_last_name = name.split(" ", 1)[-1]
+                if len(partner_last_name.split(" ")) > 1:
+                    split_partner_last_name = partner_last_name.split(" ")
+                    partner_last_name_prefix = " ".join(split_partner_last_name[:-1])
+                    partner_last_name = split_partner_last_name[-1]
+            elif aanduiding_naamgebruik == "Partner voor eigen":
+                last_name = name.split(" ", 1)[-1]
+                partner_last_name, last_name = last_name.split("-")
+                if len(last_name.split(" ")) > 1:
+                    split_last_name = last_name.split(" ")
+                    last_name_prefix = " ".join(split_last_name[:-1])
+                    last_name = split_last_name[-1]
+                if len(partner_last_name.split(" ")) > 1:
+                    split_partner_last_name = partner_last_name.split(" ")
+                    partner_last_name_prefix = " ".join(split_partner_last_name[:-1])
+                    partner_last_name = split_partner_last_name[-1]
+
+            result = get_aanschrijfwijze(
+                last_name_prefix,
+                last_name,
+                voornamen,
+                partner_last_name_prefix,
+                partner_last_name,
+                aanduiding_naamgebruik_to_enumeration[aanduiding_naamgebruik],
+                None,
+                title,
+                None,
+            )
+
+            self.assertEqual(aanschrijfwijze, result)
