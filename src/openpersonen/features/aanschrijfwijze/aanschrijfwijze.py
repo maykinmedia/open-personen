dd2dc1b3902c8b9b511610ecfbc77aac671e898a
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-27 14:30:10 +0100

    :sparkles: Implement partner having a title scenario

--- src/openpersonen/features/aanschrijfwijze/aanschrijfwijze.py
+++ src/openpersonen/features/aanschrijfwijze/aanschrijfwijze.py
@@ -38,6 +38,26 @@ def get_aanschrijfwijze(
                 aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name_prefix} {partner_last_name}-{title.lower()} {last_name_prefix} {last_name}"
             else:
                 aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {partner_last_name}-{title.lower()} {last_name}"
+    elif partner_title in ['Baron', 'Prins'] and gender_designation == 'V' and indication_name_use != 'E':
+        if partner_title == 'Baron':
+            title = 'barones'
+        elif partner_title == 'Prins':
+            title = 'prinses'
+        if indication_name_use == 'N':
+            if last_name_prefix and partner_last_name_prefix:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {last_name_prefix} {last_name}-{title} {partner_last_name_prefix} {partner_last_name}"
+            else:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {last_name}-{title} {partner_last_name}"
+        elif indication_name_use == 'P':
+            if partner_last_name_prefix:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {title} {partner_last_name_prefix} {partner_last_name}"
+            else:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {title} {partner_last_name}"
+        elif indication_name_use == 'V':
+            if last_name_prefix and partner_last_name_prefix:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {title} {partner_last_name_prefix} {partner_last_name}-{last_name_prefix} {last_name}"
+            else:
+                aanschrijfwijze = f"{get_aanschrijfwijze_first_name(first_name)} {title} {partner_last_name}-{last_name}"
     else:
         if indication_name_use == 'E':
             if last_name_prefix:
