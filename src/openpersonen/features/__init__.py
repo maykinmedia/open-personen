28b20c42c6d384d0e619fdfb73ec88842cf4fed5
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-11-30 14:23:02 +0100

    :construction: Set up files for gebruik in lopende tekst feature

--- src/openpersonen/features/__init__.py
+++ src/openpersonen/features/__init__.py
@@ -1,2 +1,5 @@
 from .aanhef.aanhef import get_aanhef
 from .aanschrijfwijze.aanschrijfwijze import get_aanschrijfwijze
+from .gebruik_in_lopende_tekst.gebruik_in_lopende_tekst import (
+    get_gebruik_in_lopende_tekst,
+)
---
+++ src/openpersonen/features/gebruik_in_lopende_tekst/gebruik_in_lopende_tekst.py
@@ -0,0 +1,15 @@
+def get_gebruik_in_lopende_tekst(
+    last_name_prefix,
+    last_name,
+    first_name,
+    partner_last_name_prefix,
+    partner_last_name,
+    indication_name_use,
+    gender_designation,
+    title,
+    partner_title,
+):
+
+    gebruik_in_lopende_tekst = "string"
+
+    return gebruik_in_lopende_tekst
