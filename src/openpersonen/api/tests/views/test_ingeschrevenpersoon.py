dc25a48a40dd24b87077972d9f34631af0d3fb94
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-12-01 16:16:47 +0100

    :recycle: Minor updates

--- src/openpersonen/api/tests/views/test_ingeschrevenpersoon.py
+++ src/openpersonen/api/tests/views/test_ingeschrevenpersoon.py
@@ -148,7 +148,6 @@ class TestIngeschrevenPersoon(APITestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTrue(post_mock.called)
-        self.maxDiff = None
         self.assertEqual(response.json(), INGESCHREVEN_PERSOON_RETRIEVE_DATA)
 
     @freeze_time("2020-09-12")
