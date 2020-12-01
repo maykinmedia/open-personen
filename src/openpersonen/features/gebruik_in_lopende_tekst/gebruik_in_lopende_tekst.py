6099e6a2d994061d9c0f9e4101df11ddbb7203c5
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-12-01 10:20:37 +0100

    :sparkles: Add getting gebruik in lopende tekst with a title

--- src/openpersonen/features/gebruik_in_lopende_tekst/gebruik_in_lopende_tekst.py
+++ src/openpersonen/features/gebruik_in_lopende_tekst/gebruik_in_lopende_tekst.py
@@ -1,17 +1,58 @@
 from openpersonen.features.constants import *
 
 
-def get_gebruik_in_lopende_tekst(
+def get_gebruik_in_lopende_tekst_with_title(
+    last_name_prefix,
+    last_name,
+    partner_last_name_prefix,
+    partner_last_name,
+    indication_name_use,
+    title,
+):
+    gebruik_in_lopende_tekst = title.lower()
+
+    if indication_name_use == EIGEN:
+        if last_name_prefix:
+            gebruik_in_lopende_tekst += f" {last_name_prefix}"
+        gebruik_in_lopende_tekst += f" {last_name}"
+    if indication_name_use == PARTNER_NA_EIGEN:
+        if last_name_prefix:
+            gebruik_in_lopende_tekst += f" {last_name_prefix}"
+        if last_name:
+            gebruik_in_lopende_tekst += f" {last_name}-"
+        else:
+            gebruik_in_lopende_tekst += f" "
+        if partner_last_name_prefix:
+            gebruik_in_lopende_tekst += f"{partner_last_name_prefix} "
+        if partner_last_name:
+            gebruik_in_lopende_tekst += f"{partner_last_name}"
+    if indication_name_use == PARTNER:
+        if partner_last_name_prefix:
+            gebruik_in_lopende_tekst += f" {partner_last_name_prefix}"
+        gebruik_in_lopende_tekst += f" {partner_last_name}"
+    if indication_name_use == PARTNER_VOOR_EIGEN:
+        if partner_last_name_prefix:
+            gebruik_in_lopende_tekst += f" {partner_last_name_prefix}"
+        if partner_last_name:
+            gebruik_in_lopende_tekst += f" {partner_last_name}-"
+        else:
+            gebruik_in_lopende_tekst += f" "
+        if last_name_prefix:
+            gebruik_in_lopende_tekst += f"{last_name_prefix} "
+        if last_name:
+            gebruik_in_lopende_tekst += f"{last_name}"
+
+    return gebruik_in_lopende_tekst
+
+
+def get_default_gebruik_in_lopende_tekst(
     last_name_prefix,
     last_name,
     partner_last_name_prefix,
     partner_last_name,
     indication_name_use,
     gender_designation,
-    title,
-    partner_title,
 ):
-
     gebruik_in_lopende_tekst = ""
 
     if gender_designation == MALE:
@@ -49,5 +90,38 @@ def get_gebruik_in_lopende_tekst(
             gebruik_in_lopende_tekst += f"{last_name_prefix} "
         if last_name:
             gebruik_in_lopende_tekst += f"{last_name}"
+
+    return gebruik_in_lopende_tekst
+
+
+def get_gebruik_in_lopende_tekst(
+    last_name_prefix,
+    last_name,
+    partner_last_name_prefix,
+    partner_last_name,
+    indication_name_use,
+    gender_designation,
+    title,
+    partner_title,
+):
+
+    if title and title not in [GRAAF, GRAVIN]:
+        gebruik_in_lopende_tekst = get_gebruik_in_lopende_tekst_with_title(
+            last_name_prefix,
+            last_name,
+            partner_last_name_prefix,
+            partner_last_name,
+            indication_name_use,
+            title,
+        )
+    else:
+        gebruik_in_lopende_tekst = get_default_gebruik_in_lopende_tekst(
+            last_name_prefix,
+            last_name,
+            partner_last_name_prefix,
+            partner_last_name,
+            indication_name_use,
+            gender_designation,
+        )
 
     return gebruik_in_lopende_tekst
