b15677d8718ec8a5c3e2caec56652a4246df8f05
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-12-03 13:47:20 +0100

    :sparkles: Use getting country code in stuf bg ingeschreven persoon converter

--- src/openpersonen/features/country_code/migrations/0001_initial.py
+++ src/openpersonen/features/country_code/migrations/0001_initial.py
@@ -7,19 +7,26 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='CountryCode',
+            name="CountryCode",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('code', models.IntegerField()),
-                ('omschrijving', models.CharField(max_length=200)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("code", models.IntegerField()),
+                ("omschrijving", models.CharField(max_length=200)),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
     ]
