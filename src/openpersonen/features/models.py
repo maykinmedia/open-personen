1a2190bc83bbe9e4164aa1dbff6110cad693b008
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-12-03 12:04:15 +0100

    :sparkles: Add helper methods for getting the code and omschrijving

--- src/openpersonen/features/models.py
+++ src/openpersonen/features/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
 
@@ -7,3 +8,17 @@ class CodeAndOmschrijving(models.Model):
 
     class Meta:
         abstract = True
+
+    @classmethod
+    def get_code_from_omschrijving(cls, omschrijving):
+        try:
+            return cls.objects.get(omschrijving=omschrijving).code
+        except ObjectDoesNotExist:
+            return 0
+
+    @classmethod
+    def get_omschrijving_from_code(cls, code):
+        try:
+            return cls.objects.get(code=code).omschrijving
+        except ObjectDoesNotExist:
+            return 'Onbekend'
