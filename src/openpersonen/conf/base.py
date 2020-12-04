96f4cc1da001f7eb84fa77211b8ef5f7cc5a3ca3
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-12-04 15:10:50 +0100

    :sparkles: Add model for storing Reden Codes and Omschrijving

--- src/openpersonen/conf/base.py
+++ src/openpersonen/conf/base.py
@@ -102,6 +102,7 @@ INSTALLED_APPS = [
     "openpersonen.contrib.stufbg",
     "openpersonen.features.country_code_and_omschrijving",
     "openpersonen.features.gemeente_code_and_omschrijving",
+    "openpersonen.features.reden_code_and_omschrijving",
     "openpersonen.token",
     "openpersonen.utils",
 ]
